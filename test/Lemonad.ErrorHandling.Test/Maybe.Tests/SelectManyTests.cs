d6d886d612c328f479a78ce283114281ca645dff
Robert Andersson <inputfalken@gmail.com>
2018-06-03 21:23:48 +0200

    Add tests with IEnumerable

---
+++ test/Lemonad.ErrorHandling.Test/Maybe.Tests/SelectManyTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Lemonad.ErrorHandling.Test.Maybe.Tests {
+    public class SelectManyTests {
+        [Fact]
+        public void Enumerable_ResultSelector_SelectMany_FlatMap_Maybe_With_Value() {
+            const string input = "hello";
+            var list = Enumerable
+                .Range(0, 3)
+                .SelectMany(_ => input.NoneWhenStringIsNullOrEmpty(), (x, y) => x + y.Length)
+                .ToList();
+
+            Assert.Equal(5, list[0]);
+            Assert.Equal(6, list[1]);
+            Assert.Equal(7, list[2]);
+        }
+        
+        [Fact]
+        public void Enumerable_ResultSelector_SelectMany_FlatMap_Maybe_Without_Value() {
+            const string input = "hello";
+            var list = Enumerable
+                .Range(0, 3)
+                .SelectMany(_ => input.SomeWhen(y => y.Length > 10), (x, y) => x + y.Length)
+                .ToList();
+
+            Assert.False(list.Any());
+        }
+    }
+}
