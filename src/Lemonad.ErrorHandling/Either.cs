305b701b2e5c109afc02308542f3823c3aab6bec
Robert Andersson <inputfalken@gmail.com>
2018-06-03 22:29:08 +0200

    Add Either API

--- src/Lemonad.ErrorHandling/Either.cs
+++ src/Lemonad.ErrorHandling/Either.cs
@@ -1,3 +1,59 @@
-﻿namespace Lemonad.ErrorHandling {
-    public class Either<TLeft, TRight> { }
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lemonad.ErrorHandling {
+    public struct Either<TLeft, TRight> : IEquatable<Either<TLeft, TRight>>, IComparable<Either<TLeft, TRight>> {
+        public bool HasValue { get; }
+
+        internal static Either<TLeft, TRight> Identity { get; } =
+            new Either<TLeft, TRight>(default(TLeft), default(TRight), false);
+
+        internal Either(TLeft left, TRight right, bool hasValue) {
+            Left = left;
+            HasValue = hasValue;
+            Right = right;
+        }
+
+        internal TRight Right { get; }
+        internal TLeft Left { get; }
+
+        public bool Equals(Either<TLeft, TRight> other) {
+            if (!HasValue && !other.HasValue)
+                return EqualityComparer<TLeft>.Default.Equals(Left, other.Left);
+
+            if (HasValue && other.HasValue)
+                return EqualityComparer<TRight>.Default.Equals(Right, other.Right);
+
+            return false;
+        }
+
+        public static bool operator ==(Either<TLeft, TRight> left, Either<TLeft, TRight> right) => left.Equals(right);
+        public static bool operator !=(Either<TLeft, TRight> left, Either<TLeft, TRight> right) => !left.Equals(right);
+
+        public static bool operator <(Either<TLeft, TRight> left, Either<TLeft, TRight> right) =>
+            left.CompareTo(right) < 0;
+
+        public static bool operator <=(Either<TLeft, TRight> left, Either<TLeft, TRight> right) =>
+            left.CompareTo(right) <= 0;
+
+        public static bool operator >(Either<TLeft, TRight> left, Either<TLeft, TRight> right) =>
+            left.CompareTo(right) > 0;
+
+        public static bool operator >=(Either<TLeft, TRight> left, Either<TLeft, TRight> right) =>
+            left.CompareTo(right) >= 0;
+
+        public override bool Equals(object obj) => obj is Either<TLeft, TRight> option && Equals(option);
+
+        public override int GetHashCode() =>
+            !HasValue ? (Left == null ? 0 : Left.GetHashCode()) : (Right == null ? 1 : Right.GetHashCode());
+
+        public int CompareTo(Either<TLeft, TRight> other) {
+            if (HasValue && !other.HasValue) return 1;
+            if (!HasValue && other.HasValue) return -1;
+
+            return HasValue
+                ? Comparer<TRight>.Default.Compare(Right, other.Right)
+                : Comparer<TLeft>.Default.Compare(Left, other.Left);
+        }
+    }
 }
--- src/Lemonad.ErrorHandling/EitherFunctions.cs
+++ src/Lemonad.ErrorHandling/EitherFunctions.cs
@@ -1,5 +1,9 @@
-﻿namespace Lemonad.ErrorHandling {
+﻿using System.Diagnostics.Contracts;
+
+namespace Lemonad.ErrorHandling {
     public static class Either {
-        
+        [Pure]
+        public static Either<TLeft, TRight> Left<TLeft, TRight>(TLeft left) =>
+            new Either<TLeft, TRight>(left, default(TRight), false);
     }
 }
