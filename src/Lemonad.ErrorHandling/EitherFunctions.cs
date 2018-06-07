a97581c47a72fb754747cdcc0e7bb93f08747d35
Robert Andersson <inputfalken@gmail.com>
2018-06-08 00:45:54 +0200

    Move parsing methods into extension class as nested classes.

--- src/Lemonad.ErrorHandling/EitherFunctions.cs
+++ src/Lemonad.ErrorHandling/EitherFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Linq;
 
 namespace Lemonad.ErrorHandling {
@@ -90,5 +91,86 @@ namespace Lemonad.ErrorHandling {
             Func<TRightSource, TRightResult> rightSelector) => source.IsLeft
             ? Left<TLeftResult, TRightResult>(leftSelector(source.Left))
             : Right<TLeftResult, TRightResult>(rightSelector(source.Right));
+
+        public static class Parse {
+            [Pure]
+            private static string FormatMessage(string input) => $"Could not parse string '{input}'.";
+
+            public static Either<string, TEnum> Enum<TEnum>(string input) where TEnum : struct =>
+                System.Enum.TryParse<TEnum>(input, out var value)
+                    ? (Either<string, TEnum>) value
+                    : FormatMessage(input);
+
+            public static Either<string, TEnum> Enum<TEnum>(string input, bool ignoreCase) where TEnum : struct =>
+                System.Enum.TryParse<TEnum>(input, ignoreCase, out var value)
+                    ? (Either<string, TEnum>) value
+                    : FormatMessage(input);
+
+            public static Either<string, decimal>
+                Decimal(string input, NumberStyles style, IFormatProvider provider) =>
+                decimal.TryParse(input, style, provider, out var number)
+                    ? (Either<string, decimal>) number
+                    : FormatMessage(input);
+
+            public static Either<string, decimal> Decimal(string input) =>
+                decimal.TryParse(input, out var number)
+                    ? (Either<string, decimal>) number
+                    : FormatMessage(input);
+
+            public static Either<string, int> Int(string input, NumberStyles style, IFormatProvider provider) =>
+                int.TryParse(input, style, provider, out var number)
+                    ? (Either<string, int>) number
+                    : FormatMessage(input);
+
+            public static Either<string, long> Long(string input, NumberStyles style, IFormatProvider provider) =>
+                long.TryParse(input, style, provider, out var number)
+                    ? (Either<string, long>) number
+                    : FormatMessage(input);
+
+            public static Either<string, long> Long(string input) =>
+                long.TryParse(input, out var number)
+                    ? (Either<string, long>) number
+                    : FormatMessage(input);
+
+            public static Either<string, int> Int(string input) => int.TryParse(input, out var number)
+                ? (Either<string, int>) number
+                : FormatMessage(input);
+
+            public static Either<string, double> Double(string input, NumberStyles style, IFormatProvider provider) =>
+                double.TryParse(input, style, provider, out var number)
+                    ? (Either<string, double>) number
+                    : FormatMessage(input);
+
+            public static Either<string, double> Double(string input) => double.TryParse(input, out var number)
+                ? (Either<string, double>) number
+                : FormatMessage(input);
+
+            public static Either<string, DateTime> DateTime(string input, DateTimeStyles style,
+                IFormatProvider provider) =>
+                System.DateTime.TryParse(input, provider, style, out var date)
+                    ? (Either<string, DateTime>) date
+                    : FormatMessage(input);
+
+            public static Either<string, DateTime> DateTimeExact(string input, string format, DateTimeStyles style,
+                IFormatProvider provider) =>
+                System.DateTime.TryParseExact(input, format, provider, style, out var date)
+                    ? (Either<string, DateTime>) date
+                    : FormatMessage(input);
+
+            public static Either<string, DateTime> DateTimeExact(string input, string[] formats, DateTimeStyles style,
+                IFormatProvider provider) =>
+                System.DateTime.TryParseExact(input, formats, provider, style, out var date)
+                    ? (Either<string, DateTime>) date
+                    : FormatMessage(input);
+
+            public static Either<string, DateTime> DateTime(string input) =>
+                System.DateTime.TryParse(input, out var date)
+                    ? (Either<string, DateTime>) date
+                    : FormatMessage(input);
+
+            public static Either<string, bool> Bool(string input) => bool.TryParse(input, out var boolean)
+                ? (Either<string, bool>) boolean
+                : FormatMessage(input);
+        }
     }
 }
--- src/Lemonad.ErrorHandling/MaybeFunctions.cs
+++ src/Lemonad.ErrorHandling/MaybeFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Linq;
 
 namespace Lemonad.ErrorHandling {
@@ -142,5 +143,83 @@ namespace Lemonad.ErrorHandling {
             Func<TSource, TSelector?> selector,
             Func<TSource, TSelector, TResult> resultSelector) where TSelector : struct => source.FlatMap(
             src => selector(src).ConvertToMaybe().Map(elem => resultSelector(src, elem)));
+
+        public static class Parse {
+            [Pure]
+            private static string FormatMessage(string input) => $"Could not parse string '{input}'.";
+
+            public static Maybe<TEnum> Enum<TEnum>(string input) where TEnum : struct =>
+                System.Enum.TryParse<TEnum>(input, out var value)
+                    ? value.Some()
+                    : Maybe.None<TEnum>();
+
+            public static Maybe<TEnum> Enum<TEnum>(string input, bool ignoreCase) where TEnum : struct =>
+                System.Enum.TryParse<TEnum>(input, ignoreCase, out var value)
+                    ? value.Some()
+                    : Maybe.None<TEnum>();
+
+            public static Maybe<decimal> Decimal(string input, NumberStyles style, IFormatProvider provider) =>
+                decimal.TryParse(input, style, provider, out var number)
+                    ? number.Some()
+                    : Maybe.None<decimal>();
+
+            public static Maybe<decimal> Decimal(string input) =>
+                decimal.TryParse(input, out var number)
+                    ? number.Some()
+                    : Maybe.None<decimal>();
+
+            public static Maybe<int> Int(string input, NumberStyles style, IFormatProvider provider) =>
+                int.TryParse(input, style, provider, out var number)
+                    ? number.Some()
+                    : Maybe.None<int>();
+
+            public static Maybe<long> Long(string input, NumberStyles style, IFormatProvider provider) =>
+                long.TryParse(input, style, provider, out var number)
+                    ? number.Some()
+                    : Maybe.None<long>();
+
+            public static Maybe<long> Long(string input) =>
+                long.TryParse(input, out var number)
+                    ? number.Some()
+                    : Maybe.None<long>();
+
+            public static Maybe<int> Int(string input) => int.TryParse(input, out var number)
+                ? number.Some()
+                : Maybe.None<int>();
+
+            public static Maybe<double> Double(string input, NumberStyles style, IFormatProvider provider) =>
+                double.TryParse(input, style, provider, out var number)
+                    ? number.Some()
+                    : Maybe.None<double>();
+
+            public static Maybe<double> Double(string input) => double.TryParse(input, out var number)
+                ? number.Some()
+                : Maybe.None<double>();
+
+            public static Maybe<DateTime> DateTime(string input, DateTimeStyles style, IFormatProvider provider) =>
+                System.DateTime.TryParse(input, provider, style, out var date)
+                    ? date.Some()
+                    : Maybe.None<DateTime>();
+
+            public static Maybe<DateTime> DateTimeExact(string input, string format, DateTimeStyles style,
+                IFormatProvider provider) =>
+                System.DateTime.TryParseExact(input, format, provider, style, out var date)
+                    ? date.Some()
+                    : Maybe.None<DateTime>();
+
+            public static Maybe<DateTime> DateTimeExact(string input, string[] formats, DateTimeStyles style,
+                IFormatProvider provider) =>
+                System.DateTime.TryParseExact(input, formats, provider, style, out var date)
+                    ? date.Some()
+                    : Maybe.None<DateTime>();
+
+            public static Maybe<DateTime> DateTime(string input) => System.DateTime.TryParse(input, out var date)
+                ? date.Some()
+                : Maybe.None<DateTime>();
+
+            public static Maybe<bool> Bool(string input) => bool.TryParse(input, out var boolean)
+                ? boolean.Some()
+                : Maybe.None<bool>();
+        }
     }
 }
