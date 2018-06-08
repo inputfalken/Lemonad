344ec44f8cf1969e28d0da6010dd4db413ed3ce7
Robert Andersson <inputfalken@gmail.com>
2018-06-08 19:41:34 +0200

    Improve error message from either parsers

--- src/Lemonad.ErrorHandling/EitherFunctions.cs
+++ src/Lemonad.ErrorHandling/EitherFunctions.cs
@@ -122,84 +122,84 @@ namespace Lemonad.ErrorHandling {
             : Right<TLeftResult, TRightResult>(rightSelector(source.Right));
 
         public static class Parse {
-            [Pure]
-            private static string FormatMessage(string input) => $"Could not parse string '{input}'.";
+            private static string FormatStringParserMessage<TEnum>(string input) where TEnum : struct =>
+                $"Could not parse type {typeof(string)}(\"{input}\") into {typeof(TEnum)}.";
 
             public static Either<string, TEnum> Enum<TEnum>(string input) where TEnum : struct =>
                 System.Enum.TryParse<TEnum>(input, out var value)
                     ? (Either<string, TEnum>) value
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<TEnum>(input);
 
             public static Either<string, TEnum> Enum<TEnum>(string input, bool ignoreCase) where TEnum : struct =>
                 System.Enum.TryParse<TEnum>(input, ignoreCase, out var value)
                     ? (Either<string, TEnum>) value
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<TEnum>(input);
 
             public static Either<string, decimal>
                 Decimal(string input, NumberStyles style, IFormatProvider provider) =>
                 decimal.TryParse(input, style, provider, out var number)
                     ? (Either<string, decimal>) number
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<decimal>(input);
 
             public static Either<string, decimal> Decimal(string input) =>
                 decimal.TryParse(input, out var number)
                     ? (Either<string, decimal>) number
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<decimal>(input);
 
             public static Either<string, int> Int(string input, NumberStyles style, IFormatProvider provider) =>
                 int.TryParse(input, style, provider, out var number)
                     ? (Either<string, int>) number
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<int>(input);
 
             public static Either<string, long> Long(string input, NumberStyles style, IFormatProvider provider) =>
                 long.TryParse(input, style, provider, out var number)
                     ? (Either<string, long>) number
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<long>(input);
 
             public static Either<string, long> Long(string input) =>
                 long.TryParse(input, out var number)
                     ? (Either<string, long>) number
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<long>(input);
 
             public static Either<string, int> Int(string input) => int.TryParse(input, out var number)
                 ? (Either<string, int>) number
-                : FormatMessage(input);
+                : FormatStringParserMessage<int>(input);
 
             public static Either<string, double> Double(string input, NumberStyles style, IFormatProvider provider) =>
                 double.TryParse(input, style, provider, out var number)
                     ? (Either<string, double>) number
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<double>(input);
 
             public static Either<string, double> Double(string input) => double.TryParse(input, out var number)
                 ? (Either<string, double>) number
-                : FormatMessage(input);
+                : FormatStringParserMessage<double>(input);
 
             public static Either<string, DateTime> DateTime(string input, DateTimeStyles style,
                 IFormatProvider provider) =>
                 System.DateTime.TryParse(input, provider, style, out var date)
                     ? (Either<string, DateTime>) date
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<DateTime>(input);
 
             public static Either<string, DateTime> DateTimeExact(string input, string format, DateTimeStyles style,
                 IFormatProvider provider) =>
                 System.DateTime.TryParseExact(input, format, provider, style, out var date)
                     ? (Either<string, DateTime>) date
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<DateTime>(input);
 
             public static Either<string, DateTime> DateTimeExact(string input, string[] formats, DateTimeStyles style,
                 IFormatProvider provider) =>
                 System.DateTime.TryParseExact(input, formats, provider, style, out var date)
                     ? (Either<string, DateTime>) date
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<DateTime>(input);
 
             public static Either<string, DateTime> DateTime(string input) =>
                 System.DateTime.TryParse(input, out var date)
                     ? (Either<string, DateTime>) date
-                    : FormatMessage(input);
+                    : FormatStringParserMessage<DateTime>(input);
 
             public static Either<string, bool> Bool(string input) => bool.TryParse(input, out var boolean)
                 ? (Either<string, bool>) boolean
-                : FormatMessage(input);
+                : FormatStringParserMessage<bool>(input);
         }
     }
 }
