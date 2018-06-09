b99ab98e40b041cdd032df0c08ae5c14353bcff2
Robert Andersson <inputfalken@gmail.com>
2018-06-09 22:43:19 +0200

    Run code cleanup

--- test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapResultSelectorTests.cs
+++ test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapResultSelectorTests.cs
@@ -5,41 +5,16 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
     public class FlatMapResultSelectorTests {
         [Fact]
         public void
-            Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_Selector__Expects_ArgumentNullException_Thrown() {
-            Func<int, Either<string, int>> selector = null;
-            Assert.Throws<ArgumentNullException>(() => {
-                var result = ErrorHandling.Either.Parse.Int("2").FlatMap(selector, (x, y) => x + y);
-                Assert.False(result.IsRight, "Either should not have a right value.");
-                Assert.True(result.IsLeft, "Either should have a left value.");
-                Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
-            });
-        }
-
-        [Fact]
-        public void
-            Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_ResultSelector__Expects_No_ArgumentNullException_Thrown() {
-            Func<int, int, int> resultSelector = null;
+            Either_String_Int__Whose_Property_IsRight_Is_False_With_Null_ResultSelector__Expects_No_ArgumentNullException_Thrown() {
+            Func<int, bool, int> resultSelector = null;
             var exception = Record.Exception(() => {
-                var result = ErrorHandling.Either.Parse.Int("2")
-                    .FlatMap(i => ErrorHandling.Either.Parse.Int("foo"), resultSelector);
+                var result = ErrorHandling.Either.Parse.Int("foo")
+                    .FlatMap(i => ErrorHandling.Either.Parse.Bool("x"), resultSelector);
                 Assert.False(result.IsRight, "Either should not have a right value.");
                 Assert.True(result.IsLeft, "Either should have a left value.");
                 Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
             });
             Assert.Null(exception);
-        }
-
-        [Fact]
-        public void
-            Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_Selector_And_Null_ResultSelector__Expects_ArgumentNullException_Thrown() {
-            Func<int, Either<string, int>> selector = null;
-            Func<int, int, int> resultSelector = null;
-            Assert.Throws<ArgumentNullException>(() => {
-                var result = ErrorHandling.Either.Parse.Int("2").FlatMap(selector, resultSelector);
-                Assert.False(result.IsRight, "Either should not have a right value.");
-                Assert.True(result.IsLeft, "Either should have a left value.");
-                Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
-            });
         }
 
         [Fact]
@@ -48,20 +23,6 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
             Func<int, Either<string, int>> selector = null;
             var exception = Record.Exception(() => {
                 var result = ErrorHandling.Either.Parse.Int("foo").FlatMap(selector, (x, y) => x + y);
-                Assert.False(result.IsRight, "Either should not have a right value.");
-                Assert.True(result.IsLeft, "Either should have a left value.");
-                Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
-            });
-            Assert.Null(exception);
-        }
-
-        [Fact]
-        public void
-            Either_String_Int__Whose_Property_IsRight_Is_False_With_Null_ResultSelector__Expects_No_ArgumentNullException_Thrown() {
-            Func<int, bool, int> resultSelector = null;
-            var exception = Record.Exception(() => {
-                var result = ErrorHandling.Either.Parse.Int("foo")
-                    .FlatMap(i => ErrorHandling.Either.Parse.Bool("x"), resultSelector);
                 Assert.False(result.IsRight, "Either should not have a right value.");
                 Assert.True(result.IsLeft, "Either should have a left value.");
                 Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
@@ -85,22 +46,48 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
 
         [Fact]
         public void
-            Either_String_Int_Whose_Property_IsRight_Is_True__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_True() {
-            var intParse = ErrorHandling.Either.Parse.Int("2");
-            var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
-
-            var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
-            Assert.True(result.IsRight, "Either should have a right value.");
-            Assert.False(result.IsLeft, "Either should not have a left value.");
-            Assert.Equal(default(string), result.Left);
-            Assert.Equal(4.2, result.Right);
+            Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_ResultSelector__Expects_No_ArgumentNullException_Thrown() {
+            Func<int, int, int> resultSelector = null;
+            var exception = Record.Exception(() => {
+                var result = ErrorHandling.Either.Parse.Int("2")
+                    .FlatMap(i => ErrorHandling.Either.Parse.Int("foo"), resultSelector);
+                Assert.False(result.IsRight, "Either should not have a right value.");
+                Assert.True(result.IsLeft, "Either should have a left value.");
+                Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            });
+            Assert.Null(exception);
         }
 
         [Fact]
         public void
-            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_False() {
+            Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_Selector__Expects_ArgumentNullException_Thrown() {
+            Func<int, Either<string, int>> selector = null;
+            Assert.Throws<ArgumentNullException>(() => {
+                var result = ErrorHandling.Either.Parse.Int("2").FlatMap(selector, (x, y) => x + y);
+                Assert.False(result.IsRight, "Either should not have a right value.");
+                Assert.True(result.IsLeft, "Either should have a left value.");
+                Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            });
+        }
+
+        [Fact]
+        public void
+            Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_Selector_And_Null_ResultSelector__Expects_ArgumentNullException_Thrown() {
+            Func<int, Either<string, int>> selector = null;
+            Func<int, int, int> resultSelector = null;
+            Assert.Throws<ArgumentNullException>(() => {
+                var result = ErrorHandling.Either.Parse.Int("2").FlatMap(selector, resultSelector);
+                Assert.False(result.IsRight, "Either should not have a right value.");
+                Assert.True(result.IsLeft, "Either should have a left value.");
+                Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            });
+        }
+
+        [Fact]
+        public void
+            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
             var intParse = ErrorHandling.Either.Parse.Int("foo");
-            var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
 
             var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
             Assert.False(result.IsRight, "Either should have not a right value.");
@@ -111,9 +98,9 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
 
         [Fact]
         public void
-            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
+            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_False() {
             var intParse = ErrorHandling.Either.Parse.Int("foo");
-            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
+            var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
 
             var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
             Assert.False(result.IsRight, "Either should have not a right value.");
@@ -133,6 +120,19 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
             Assert.True(result.IsLeft, "Either should have a left value.");
             Assert.Equal("Could not parse type System.String(\"foo\") into System.Double.", result.Left);
             Assert.Equal(default(double), result.Right);
+        }
+
+        [Fact]
+        public void
+            Either_String_Int_Whose_Property_IsRight_Is_True__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_True() {
+            var intParse = ErrorHandling.Either.Parse.Int("2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
+
+            var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
+            Assert.True(result.IsRight, "Either should have a right value.");
+            Assert.False(result.IsLeft, "Either should not have a left value.");
+            Assert.Equal(default(string), result.Left);
+            Assert.Equal(4.2, result.Right);
         }
     }
 }
--- test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapTests.cs
+++ test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapTests.cs
@@ -15,7 +15,7 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
             });
             Assert.Null(exception);
         }
-        
+
         [Fact]
         public void
             Either_String_Int__Whose_Property_IsRight_Is_True_With_Null_Selector__Expects_ArgumentNullException_Thrown() {
@@ -40,6 +40,19 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
 
         [Fact]
         public void
+            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
+            var intParse = ErrorHandling.Either.Parse.Int("foo");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
+
+            var result = intParse.FlatMap(x => doubleParse);
+            Assert.False(result.IsRight, "Either should not have a right value.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            Assert.Equal(default(double), result.Right);
+        }
+
+        [Fact]
+        public void
             Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_Selector_To_Never_Be_Invoked() {
             var intParse = ErrorHandling.Either.Parse.Int("foo");
             var doubleParse = ErrorHandling.Either.Parse.Double("2");
@@ -51,6 +64,19 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
             });
 
             Assert.False(isInvoked, "Selector should never have been invoked.");
+        }
+
+        [Fact]
+        public void
+            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_False() {
+            var intParse = ErrorHandling.Either.Parse.Int("foo");
+            var doubleParse = ErrorHandling.Either.Parse.Double("2");
+
+            var result = intParse.FlatMap(x => doubleParse);
+            Assert.False(result.IsRight, "Either should not have a right value.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            Assert.Equal(default(double), result.Right);
         }
 
         [Fact]
@@ -70,6 +96,19 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
 
         [Fact]
         public void
+            Either_String_Int_Whose_Property_IsRight_Is_True__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
+            var intParse = ErrorHandling.Either.Parse.Int("2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
+
+            var result = intParse.FlatMap(x => doubleParse);
+            Assert.False(result.IsRight, "Either should not have a right value.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Double.", result.Left);
+            Assert.Equal(default(double), result.Right);
+        }
+
+        [Fact]
+        public void
             Either_String_Int_Whose_Property_IsRight_Is_True__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_Selector_To_Be_Invoked() {
             var intParse = ErrorHandling.Either.Parse.Int("2");
             var doubleParse = ErrorHandling.Either.Parse.Double("2");
@@ -81,45 +120,6 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
             });
 
             Assert.True(isInvoked, "Selector should be invoked.");
-        }
-
-        [Fact]
-        public void
-            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_False() {
-            var intParse = ErrorHandling.Either.Parse.Int("foo");
-            var doubleParse = ErrorHandling.Either.Parse.Double("2");
-
-            var result = intParse.FlatMap(x => doubleParse);
-            Assert.False(result.IsRight, "Either should not have a right value.");
-            Assert.True(result.IsLeft, "Either should have a left value.");
-            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
-            Assert.Equal(default(double), result.Right);
-        }
-
-        [Fact]
-        public void
-            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
-            var intParse = ErrorHandling.Either.Parse.Int("foo");
-            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
-
-            var result = intParse.FlatMap(x => doubleParse);
-            Assert.False(result.IsRight, "Either should not have a right value.");
-            Assert.True(result.IsLeft, "Either should have a left value.");
-            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
-            Assert.Equal(default(double), result.Right);
-        }
-
-        [Fact]
-        public void
-            Either_String_Int_Whose_Property_IsRight_Is_True__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
-            var intParse = ErrorHandling.Either.Parse.Int("2");
-            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
-
-            var result = intParse.FlatMap(x => doubleParse);
-            Assert.False(result.IsRight, "Either should not have a right value.");
-            Assert.True(result.IsLeft, "Either should have a left value.");
-            Assert.Equal("Could not parse type System.String(\"foo\") into System.Double.", result.Left);
-            Assert.Equal(default(double), result.Right);
         }
 
         [Fact]
