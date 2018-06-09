bcd98e924c9faa13af974230b485aec61c213cda
Robert Andersson <inputfalken@gmail.com>
2018-06-09 13:24:03 +0200

    Add resultselector flatmap tests

--- test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapResultSelectorTests.cs
+++ test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapResultSelectorTests.cs
@@ -17,41 +17,41 @@ namespace Lemonad.ErrorHandling.Test.Either.Tests {
         
         [Fact]
         public void
-            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_True() {
+            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_True__Expects_String_Double_Whose_Property_IsRight_Is_False() {
             var intParse = ErrorHandling.Either.Parse.Int("foo");
             var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
 
             var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
-            Assert.True(result.IsRight, "Either should have a right value.");
-            Assert.False(result.IsLeft, "Either should not have a left value.");
-            Assert.Equal(default(string), result.Left);
-            Assert.Equal(4.2, result.Right);
+            Assert.False(result.IsRight, "Either should have not a right value.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            Assert.Equal(default(double), result.Right);
         }
         
         [Fact]
         public void
-            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_True() {
+            Either_String_Int_Whose_Property_IsRight_Is_False__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_False() {
             var intParse = ErrorHandling.Either.Parse.Int("foo");
-            var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
 
             var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
-            Assert.True(result.IsRight, "Either should have a right value.");
-            Assert.False(result.IsLeft, "Either should not have a left value.");
-            Assert.Equal(default(string), result.Left);
-            Assert.Equal(4.2, result.Right);
+            Assert.False(result.IsRight, "Either should have not a right value.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            Assert.Equal(default(double), result.Right);
         }
         
         [Fact]
         public void
             Either_String_Int_Whose_Property_IsRight_Is_True__FLatmaps_Either_String_Double_Whose_Property_IsRight_Is_False__Expects_String_Double_Whose_Property_IsRight_Is_True() {
-            var intParse = ErrorHandling.Either.Parse.Int("foo");
-            var doubleParse = ErrorHandling.Either.Parse.Double("2.2");
+            var intParse = ErrorHandling.Either.Parse.Int("2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
 
             var result = intParse.FlatMap(x => doubleParse, (i, d) => i + d);
-            Assert.True(result.IsRight, "Either should have a right value.");
-            Assert.False(result.IsLeft, "Either should not have a left value.");
-            Assert.Equal(default(string), result.Left);
-            Assert.Equal(4.2, result.Right);
+            Assert.False(result.IsRight, "Either should have not a right value.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Double.", result.Left);
+            Assert.Equal(default(double), result.Right);
         }
     }
 }
