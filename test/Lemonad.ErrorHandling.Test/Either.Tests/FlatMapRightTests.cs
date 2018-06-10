ba6d286b13ab1849db020b8d09b3833ea723441a
Robert Andersson <inputfalken@gmail.com>
2018-06-10 15:16:54 +0200

    Add tests for flatmapright

---
+++ test/Lemonad.ErrorHandling.Test/Either.Tests/FlatMapRightTests.cs
@@ -0,0 +1,97 @@
+﻿using Xunit;
+
+namespace Lemonad.ErrorHandling.Test.Either.Tests {
+    public class FlatMapRightTests {
+        [Fact]
+        public void
+            Either_String_Int__Whose_Property_IsRight_Is_False__Flatmaps__Either_String_Double__Whose_Property_IsRight_Is_False__Expects_Either_String_Double__Whose_Property_IsRight_Is_False__With_Left_Message_From_First_either() {
+            var intParse = ErrorHandling.Either.Parse.Int("foo");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
+            var rightExecuted = false;
+            var leftExectued = false;
+            var result = intParse.FlatMapRight(i => {
+                rightExecuted = true;
+                return doubleParse;
+            }, s => {
+                leftExectued = true;
+                return "Failure" + s;
+            });
+
+            Assert.False(rightExecuted, "Right should not get exectued since intparse did not succeed.");
+            Assert.False(leftExectued, "Left should not be exectued since intparse did not succeed.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.False(result.IsRight, "Either should not have a right value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            Assert.Equal(default(double), result.Right);
+        }
+
+        [Fact]
+        public void
+            Either_String_Int__Whose_Property_IsRight_Is_True__Flatmaps__Either_String_Double__Whose_Property_IsRight_Is_False__Expects_Either_String_Double__Whose_Property_IsRight_Is_False__With_Left_Message_From_Second_either() {
+            var intParse = ErrorHandling.Either.Parse.Int("2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("foo");
+            var rightExecuted = false;
+            var leftExectued = false;
+            var result = intParse.FlatMapRight(i => {
+                rightExecuted = true;
+                return doubleParse;
+            }, s => {
+                leftExectued = true;
+                return "Failure " + s;
+            });
+
+            Assert.True(rightExecuted, "Right should get exectued since intparse did succeed.");
+            Assert.True(leftExectued, "Left should get exectued since intparse did succeed.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.False(result.IsRight, "Either should not have a right value.");
+            Assert.Equal("Failure Could not parse type System.String(\"foo\") into System.Double.", result.Left);
+            Assert.Equal(default(double), result.Right);
+        }
+
+        [Fact]
+        public void
+            Either_String_Int__Whose_Property_IsRight_Is_False__Flatmaps__Either_String_Double__Whose_Property_IsRight_Is_False__Expects_Either_String_Double__Whose_Property_IsRight_Is_True__With_Left_Message_From_Second_either() {
+            var rightExecuted = false;
+            var leftExectued = false;
+            var intParse = ErrorHandling.Either.Parse.Int("foo");
+            var doubleParse = ErrorHandling.Either.Parse.Double("2");
+            var result = intParse.FlatMapRight(i => {
+                rightExecuted = true;
+                return doubleParse;
+            }, s => {
+                leftExectued = true;
+                return "Failure " + s;
+            });
+
+            Assert.False(rightExecuted, "Right should not get exectued since intparse did not succeed.");
+            Assert.False(leftExectued, "Left should not be exectued since intparse did not succeed.");
+            Assert.True(result.IsLeft, "Either should have a left value.");
+            Assert.False(result.IsRight, "Either should not have a right value.");
+            Assert.Equal("Could not parse type System.String(\"foo\") into System.Int32.", result.Left);
+            Assert.Equal(default(double), result.Right);
+        }
+
+        [Fact]
+        public void
+            Either_String_Int__Whose_Property_IsRight_Is_False__Flatmaps__Either_String_Double__Whose_Property_True_Is_False__Expects_Either_String_Double__Whose_Property_IsRight_Is_True__With_Left_Message_From_Second_either() {
+            var rightExecuted = false;
+            var leftExectued = false;
+            var intParse = ErrorHandling.Either.Parse.Int("2");
+            var doubleParse = ErrorHandling.Either.Parse.Double("2");
+            var result = intParse.FlatMapRight(i => {
+                rightExecuted = true;
+                return doubleParse;
+            }, s => {
+                leftExectued = true;
+                return "Failure " + s;
+            });
+
+            Assert.True(rightExecuted, "Right should get exectued since intparse succeded..");
+            Assert.False(leftExectued, "Left should not be exectued since both intparse and doubleparse succeded.");
+            Assert.False(result.IsLeft, "Either should not have a left value.");
+            Assert.True(result.IsRight, "Either should have a right value.");
+            Assert.Equal(default(string), result.Left);
+            Assert.Equal(2, result.Right);
+        }
+    }
+}
