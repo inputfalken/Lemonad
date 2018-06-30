2b6e7f37ce0523818a224ebc4685c79c08dd20c6
Robert Andersson <robert.andersson.swe@gmail.com>
2018-06-30 12:58:24 +0200

    Add console sample project

---
+++ samples/ConsoleInputValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Lemonad.ErrorHandling;
+
+namespace ConsoleInputValidation {
+    internal static class Program {
+        private static int Main(string[] args) {
+            Console.WriteLine("Please supply your name.");
+            return Console.ReadLine()
+                .ToResult<string, ExitCode>()
+                .IsErrorWhen(string.IsNullOrWhiteSpace, () => ExitCode.EmptyName)
+                .Flatten(OnlyContainsAlphaNumericChars)
+                .FullMap(_ => (int) ExitCode.Success, code => (int) code)
+                .DoWithError(x => Console.WriteLine($"Bad input, exiting with code: {x}"))
+                .DoWith(x => Console.WriteLine($"Good input, exitiing with code: {x}"))
+                .Match(x => x, x => x);
+        }
+
+        private static Result<string, ExitCode> OnlyContainsAlphaNumericChars(string input) {
+            if (input.All(c => char.IsLetter(c) || char.IsNumber(c)))
+                return input;
+            return ExitCode.NameContainsNoneAlphaNumericChars;
+        }
+
+        private enum ExitCode {
+            Success = 0,
+            EmptyName = 1,
+            NameContainsNoneAlphaNumericChars = 2
+        }
+    }
+}
