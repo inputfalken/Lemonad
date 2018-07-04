7aee4a6dcc0e8ab7de02dc2fbca720bf1e6cb8a1
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-04 20:57:46 +0200

    Use in keyword for parameters

--- src/Lemonad.ErrorHandling/Maybe.cs
+++ src/Lemonad.ErrorHandling/Maybe.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 
 namespace Lemonad.ErrorHandling {
-    public struct Maybe<T> : IEquatable<Maybe<T>>, IComparable<Maybe<T>> {
-        public readonly static Maybe<T> None { get; } = new Maybe<T>(default(T), false);
+    public readonly struct Maybe<T> : IEquatable<Maybe<T>>, IComparable<Maybe<T>> {
+        public static Maybe<T> None { get; } = new Maybe<T>(default(T), false);
 
         /// <summary>
         /// Is true if there's a <typeparamref name="T"/> in the current state of the <see cref="Maybe{T}"/>.
@@ -20,7 +20,7 @@ namespace Lemonad.ErrorHandling {
         internal T Value { get; }
 
         // TODO add IEqualityComparer ctor overload.
-        internal Maybe(T value, bool hasValue) {
+        internal Maybe(in T value, in bool hasValue) {
             Value = value;
             HasValue = hasValue;
         }
