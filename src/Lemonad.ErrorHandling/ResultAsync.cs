5f6e5d94e433d59db5ad35fa76fc6688664e68b5
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-05 22:26:59 +0200

    Add implicit operators for value and errors

--- src/Lemonad.ErrorHandling/ResultAsync.cs
+++ src/Lemonad.ErrorHandling/ResultAsync.cs
@@ -12,12 +12,23 @@ namespace Lemonad.ErrorHandling {
     /// The type which is considered as failure.
     /// </typeparam>
     public struct ResultAsync<T, TError> {
-        private readonly Task<Result<T, TError>> _result;
-
-        public ResultAsync(Task<Result<T, TError>> result) => _result = result;
-
         public static implicit operator ResultAsync<T, TError>(Task<Result<T, TError>> value) =>
             new ResultAsync<T, TError>(value);
+
+        public static implicit operator ResultAsync<T, TError>(T value) => Task.FromResult(value);
+        public static implicit operator ResultAsync<T, TError>(TError error) => Task.FromResult(error);
+
+        public static implicit operator ResultAsync<T, TError>(Task<T> value) {
+            async Task<Result<T, TError>> Task() => await value.ConfigureAwait(false);
+            return Task();
+        }
+
+        public static implicit operator ResultAsync<T, TError>(Task<TError> error) {
+            async Task<Result<T, TError>> Task() => await error.ConfigureAwait(false);
+            return Task();
+        }
+
+        private readonly Task<Result<T, TError>> _result;
 
         public ResultAsync<T, TError> Filter(Func<T, bool> predicate, Func<TError> errorSelector) {
             async Task<Result<T, TError>> Task(Task<Result<T, TError>> x) =>
@@ -25,6 +36,8 @@ namespace Lemonad.ErrorHandling {
 
             return Task(_result);
         }
+
+        public ResultAsync(Task<Result<T, TError>> result) => _result = result;
 
         public ResultAsync<TResult, TError> FlatMap<TErrorResult, TFlatMap, TResult>(
             Func<T, Result<TFlatMap, TErrorResult>> flatMapSelector, Func<T, TFlatMap, TResult> resultSelector,
