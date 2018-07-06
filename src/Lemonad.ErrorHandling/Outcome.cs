43f5fa0c49884153594532d1e66e708c639b91ae
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-06 22:36:23 +0200

    Add map function

--- src/Lemonad.ErrorHandling/Outcome.cs
+++ src/Lemonad.ErrorHandling/Outcome.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
 
 namespace Lemonad.ErrorHandling {
@@ -8,6 +9,12 @@ namespace Lemonad.ErrorHandling {
     public class Outcome<T, TError> {
         private readonly Task<Result<T, TError>> _result;
 
-        public Outcome(Task<Result<T, TError>> result) => _result = result ?? throw new ArgumentNullException(nameof(result));
+        public Outcome(Task<Result<T, TError>> result) =>
+            _result = result ?? throw new ArgumentNullException(nameof(result));
+
+        [Pure]
+        public Outcome<TResult, TError> Map<TResult>(Func<T, TResult> selector) => new Outcome<TResult, TError>(
+            new Func<Task<Result<TResult, TError>>>(async () => (await _result.ConfigureAwait(false)).Map(selector))()
+        );
     }
 }
