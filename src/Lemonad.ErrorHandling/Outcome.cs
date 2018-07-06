97c3d4b5efbfe5e9438b648384dda67efe5a5e5f
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-06 20:45:48 +0200

    Add class to represent asyn Result<T, TError>

---
+++ src/Lemonad.ErrorHandling/Outcome.cs
@@ -0,0 +1,8 @@
+﻿namespace Lemonad.ErrorHandling {
+    /// <summary>
+    /// Async version of <see cref="Result{T,TError}"/>.
+    /// </summary>
+    public class Outcome<T, TError> {
+
+    }
+}
