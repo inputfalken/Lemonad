15ee2704c5d5fe1d6ceda0ab104471fcc0be6ca4
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-06 21:51:26 +0200

    Add constructor

--- src/Lemonad.ErrorHandling/Outcome.cs
+++ src/Lemonad.ErrorHandling/Outcome.cs
@@ -1,8 +1,12 @@
-﻿namespace Lemonad.ErrorHandling {
+﻿using System.Threading.Tasks;
+
+namespace Lemonad.ErrorHandling {
     /// <summary>
     /// Async version of <see cref="Result{T,TError}"/>.
     /// </summary>
     public class Outcome<T, TError> {
+        private readonly Task<Result<T, TError>> _result;
 
+        public Outcome(Task<Result<T, TError>> result) => _result = result;
     }
 }
