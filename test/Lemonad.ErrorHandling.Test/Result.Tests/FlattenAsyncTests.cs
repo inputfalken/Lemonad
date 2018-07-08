558efd2b3bff4f0f95ec178aa173c6936e734bae
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-08 18:28:21 +0200

    Add FlattenAsync tests

---
+++ test/Lemonad.ErrorHandling.Test/Result.Tests/FlattenAsyncTests.cs
@@ -0,0 +1,236 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Lemonad.ErrorHandling.Test.Result.Tests {
+    public class FlattenAsyncTests {
+        private static Result<double, string> Division(double left, double right) {
+            if (right == 0)
+                return $"Can not divide '{left}' with '{right}'.";
+
+            return left / right;
+        }
+
+        private static async Task<Result<double, string>> DivisionAsync(double left, double right) {
+            await System.Threading.Tasks.Task.Delay(50);
+
+            if (right == 0)
+                return await System.Threading.Tasks.Task.Run(() => $"Can not divide '{left}' with '{right}'.");
+            return left / right;
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Error_Flatmaps_Result_with_Error__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var errorSelectorExecuted = false;
+            var flattenAsync = Division(2, 0).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 0);
+            }, s => {
+                errorSelectorExecuted = true;
+                return s;
+            });
+            var result = await flattenAsync;
+
+            Assert.False(errorSelectorExecuted,
+                "Errorselector should not get exeuted since there is an error in the source.");
+            Assert.False(flatSelectorExecuted,
+                "The flatmap selector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Error_Flatmaps_Result_with_Error_Without_ErrorSelector__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var flattenAsync = Division(2, 0).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 0);
+            });
+            var result = await flattenAsync;
+
+            Assert.False(flatSelectorExecuted,
+                "The flatmap selector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Error_Flatmaps_Result_with_Value__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var errorSelectorExecuted = false;
+            var flatten = Division(2, 0).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 2);
+            }, s => {
+                errorSelectorExecuted = true;
+                return s;
+            });
+            var result = await flatten;
+
+            Assert.False(errorSelectorExecuted,
+                "Errorselector should not get exeuted since there is an error in the source.");
+            Assert.False(flatSelectorExecuted,
+                "The flatmap selector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Error_Flatmaps_Result_with_Value_Without_ErrorSelector__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var flattenAsync = Division(2, 0).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 2);
+            });
+
+            var result = await flattenAsync;
+
+            Assert.False(flatSelectorExecuted,
+                "The flatmap selector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Value_Flatmaps_Result_with_Error__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var errorSelectorExecuted = false;
+            var flatMap = Division(2, 2).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 0);
+            }, s => {
+                errorSelectorExecuted = true;
+                return s;
+            });
+            var result = await flatMap;
+
+            Assert.True(errorSelectorExecuted,
+                "Errorselector should not exeuted since the errror came from the result given to the flatselector.");
+            Assert.True(flatSelectorExecuted,
+                "The flatmapSelector should get exectued.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Value_Flatmaps_Result_with_Error_Without_ErrorSelector__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var flattenAsync = Division(2, 2).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 0);
+            });
+            var result = await flattenAsync;
+
+            Assert.True(flatSelectorExecuted,
+                "The flatmapSelector should get exectued.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Value_Flatmaps_Result_with_Value__Expects_Result_With_Value() {
+            var flatSelectorExecuted = false;
+            var errorSelectorExecuted = false;
+            var flattenAsync = Division(2, 2).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 2);
+            }, s => {
+                errorSelectorExecuted = true;
+                return s;
+            });
+
+            var result = await flattenAsync;
+            Assert.True(flatSelectorExecuted, "flatmapselector should get executed.");
+            Assert.False(errorSelectorExecuted, "Errorselector should not get exeuted.");
+            Assert.True(result.HasValue, "Result should have a value.");
+            Assert.False(result.HasError, "Result should not have a error.");
+            Assert.Equal(1, result.Value);
+            Assert.Equal(default, result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Value_Flatmaps_Result_with_Value_Without_ErrorSelector__Expects_Result_With_Value() {
+            var flatSelectorExecuted = false;
+            var flattenAsync = Division(2, 2).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 2);
+            });
+            var result = await flattenAsync;
+            Assert.True(flatSelectorExecuted, "flatmapselector should get executed.");
+            Assert.True(result.HasValue, "Result should have a value.");
+            Assert.False(result.HasError, "Result should not have a error.");
+            Assert.Equal(1, result.Value);
+            Assert.Equal(default, result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Value_FlatmapsRS_Result_with_Error__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var errorSelectorExecuted = false;
+            var flattenAsync = Division(2, 2).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 0);
+            }, s => {
+                errorSelectorExecuted = true;
+                return s;
+            });
+
+            var result = await flattenAsync;
+
+            Assert.True(errorSelectorExecuted,
+                "Errorselector should get exeuted since the errror came from the result given to the flatselector.");
+            Assert.True(flatSelectorExecuted);
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task
+            Result_With_Value_FlatmapsRS_Result_with_Error_Without_ErrorSelector__Expects_Result_With_Error() {
+            var flatSelectorExecuted = false;
+            var flattenAsync = Division(2, 2).FlattenAsync(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 0);
+            });
+
+            var result = await flattenAsync;
+
+            Assert.True(flatSelectorExecuted);
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+        }
+    }
+}
