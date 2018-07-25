65c036ba6e8c91abda03677a3905cd39ae57cf40
Robert Andersson <robert.andersson.swe@gmail.com>
2018-07-25 22:54:58 +0200

    Add tests for flatmap with same type of error

--- src/Lemonad.ErrorHandling/Extensions/AsynchronousResultExtensions.cs
+++ src/Lemonad.ErrorHandling/Extensions/AsynchronousResultExtensions.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace Lemonad.ErrorHandling.Extensions {
     public static class AsynchronousResultExtensions {
-        /// <inheritdoc cref="Result{T,TError}.Map{TResult}"/>
         [Pure]
         public static async Task<Result<TResult, TError>> Map<T, TResult, TError>(this Task<Result<T, TError>> source,
             Func<T, TResult> selector) => (await source.ConfigureAwait(false)).Map(selector);
 
-        /// <inheritdoc cref="Result{T,TError}.MapError{TErrorResult}"/>
         [Pure]
         public static async Task<Result<T, TErrorResult>> MapError<T, TError, TErrorResult>(
             this Task<Result<T, TError>> source, Func<TError, TErrorResult> selector) =>
             (await source.ConfigureAwait(false)).MapError(selector);
 
-        /// <inheritdoc cref="Result{T,TError}.FullMap{TResult,TErrorResult}"/>
         [Pure]
         public static async Task<Result<TResult, TErrorResult>> FullMap<T, TResult, TError, TErrorResult>(
             this Task<Result<T, TError>> source,
@@ -24,7 +20,6 @@ namespace Lemonad.ErrorHandling.Extensions {
             Func<TError, TErrorResult> errorSelector
         ) => (await source.ConfigureAwait(false)).FullMap(selector, errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.Match{TResult}"/>
         [Pure]
         public static async Task<TResult> Match<T, TResult, TError>(
             this Task<Result<T, TError>> source,
@@ -32,29 +27,24 @@ namespace Lemonad.ErrorHandling.Extensions {
             Func<TError, TResult> errorSelector) =>
             (await source.ConfigureAwait(false)).Match(selector, errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.Match"/>
         public static async Task Match<T, TError>(
             this Task<Result<T, TError>> source,
             Action<T> action,
             Action<TError> errorAction) =>
             (await source.ConfigureAwait(false)).Match(action, errorAction);
 
-        /// <inheritdoc cref="Result{T,TError}.Do"/>
         public static async Task<Result<T, TError>> Do<T, TError>(this Task<Result<T, TError>> source, Action action) =>
             (await source.ConfigureAwait(false)).Do(action);
 
-        /// <inheritdoc cref="Result{T,TError}.DoWithError"/>
         public static async Task<Result<T, TError>> DoWithError<T, TError>(
             this Task<Result<T, TError>> source,
             Action<TError> action) => (await source.ConfigureAwait(false)).DoWithError(action);
 
-        /// <inheritdoc cref="Result{T,TError}.DoWith"/>
         public static async Task<Result<T, TError>> DoWith<T, TError>(
             this Task<Result<T, TError>> source,
             Action<T> action) =>
             (await source.ConfigureAwait(false)).DoWith(action);
 
-        /// <inheritdoc cref="Result{T,TError}.Filter"/>
         [Pure]
         public static async Task<Result<T, TError>> Filter<T, TError>(
             this Task<Result<T, TError>> source,
@@ -62,7 +52,6 @@ namespace Lemonad.ErrorHandling.Extensions {
             Func<TError> errorSelector) =>
             (await source.ConfigureAwait(false)).Filter(predicate, errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.IsErrorWhen"/>
         [Pure]
         public static async Task<Result<T, TError>> IsErrorWhen<T, TError>(
             this Task<Result<T, TError>> source,
@@ -70,46 +59,39 @@ namespace Lemonad.ErrorHandling.Extensions {
             Func<TError> errorSelector) =>
             (await source.ConfigureAwait(false)).IsErrorWhen(predicate, errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.IsErrorWhenNull"/>
         [Pure]
         public static async Task<Result<T, TError>> IsErrorWhenNull<T, TError>(
             this Task<Result<T, TError>> source,
             Func<TError> errorSelector) =>
             (await source.ConfigureAwait(false)).IsErrorWhenNull(errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.CastError{TResult}"/>
         [Pure]
         internal static async Task<Result<T, TErrorResult>> CastError<T, TError, TErrorResult>
             (this Task<Result<T, TError>> source) =>
             (await source.ConfigureAwait(false)).CastError<TErrorResult>();
 
-        /// <inheritdoc cref="Result{T,TError}.FullCast{TResult,TErrorResult}"/>
         [Pure]
         internal static async Task<Result<TResult, TErrorResult>> FullCast<T, TResult, TError, TErrorResult>(
             this Task<Result<T, TError>> source) =>
             (await source.ConfigureAwait(false)).FullCast<TResult, TErrorResult>();
 
-        /// <inheritdoc cref="Result{T,TError}.Cast{TResult}"/>
         [Pure]
         internal static async Task<Result<TResult, TError>> Cast<T, TResult, TError>(
             this Task<Result<T, TError>> source) =>
             (await source.ConfigureAwait(false)).Cast<TResult>();
 
-        /// <inheritdoc cref="Result{T,TError}.SafeCast{TResult}"/>
         [Pure]
         internal static async Task<Result<TResult, TError>> SafeCast<T, TResult, TError>(
             this Task<Result<T, TError>> source,
             Func<TError> errorSelector)
             => (await source.ConfigureAwait(false)).SafeCast<TResult>(errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.Flatten{TResult,TErrorResult}(System.Func{T,Lemonad.ErrorHandling.Result{TResult,TErrorResult}},System.Func{TErrorResult,TError})"/>
         [Pure]
         public static async Task<Result<T, TError>> Flatten<T, TResult, TError, TErrorResult>(
             this Task<Result<T, TError>> source, Func<T, Result<TResult, TErrorResult>> selector,
             Func<TErrorResult, TError> errorSelector) =>
             (await source.ConfigureAwait(false)).Flatten(selector, errorSelector);
 
-        /// <inheritdoc cref="Result{T,TError}.Flatten{TResult}(System.Func{T,Lemonad.ErrorHandling.Result{TResult,TError}})"/>
         [Pure]
         public static async Task<Result<T, TError>> Flatten<T, TError, TResult>(
             this Task<Result<T, TError>> source,
@@ -119,13 +101,13 @@ namespace Lemonad.ErrorHandling.Extensions {
         public static async Task<Result<T, TError>> Flatten<T, TResult, TError, TErrorResult>(
             this Task<Result<T, TError>> source, Func<T, Task<Result<TResult, TErrorResult>>> selector,
             Func<TErrorResult, TError> errorSelector) =>
-            await (await source.ConfigureAwait(false)).Flatten(selector, errorSelector);
+            await (await source.ConfigureAwait(false)).Flatten(selector, errorSelector).ConfigureAwait(false);
 
         [Pure]
         public static async Task<Result<T, TError>> Flatten<T, TError, TResult>(
             this Task<Result<T, TError>> source,
             Func<T, Task<Result<TResult, TError>>> selector) =>
-            await (await source.ConfigureAwait(false)).Flatten(selector);
+            await (await source.ConfigureAwait(false)).Flatten(selector).ConfigureAwait(false);
 
         [Pure]
         public static async Task<Result<TResult, TErrorResult>> FullFlatMap<T, TError, TResult, TErrorResult>(
@@ -152,8 +134,8 @@ namespace Lemonad.ErrorHandling.Extensions {
         public static async Task<Result<TResult, TErrorResult>> FullFlatMap<T, TError, TFlatMap, TResult, TErrorResult>(
             this Task<Result<T, TError>> source,
             Func<T, Result<TFlatMap, TErrorResult>> flatMapSelector, Func<T, TFlatMap, TResult> resultSelector,
-            Func<TError, TErrorResult> errorSelector) => (await source.ConfigureAwait(false))
-            .FullFlatMap(flatMapSelector, resultSelector, errorSelector);
+            Func<TError, TErrorResult> errorSelector) =>
+            (await source.ConfigureAwait(false)).FullFlatMap(flatMapSelector, resultSelector, errorSelector);
 
         [Pure]
         public static async Task<Result<TResult, TError>> FlatMap<T, TResult, TError>(
@@ -179,7 +161,7 @@ namespace Lemonad.ErrorHandling.Extensions {
             this Task<Result<T, TError>> source,
             Func<T, Task<Result<TSelector, TError>>> flatSelector,
             Func<T, TSelector, TResult> resultSelector) =>
-            await (await source.ConfigureAwait(false)).FlatMap(flatSelector, resultSelector);
+            await (await source.ConfigureAwait(false)).FlatMap(flatSelector, resultSelector).ConfigureAwait(false);
 
         [Pure]
         public static async Task<Result<TResult, TError>> FlatMap<T, TResult, TError, TErrorResult>(
--- test/Lemonad.ErrorHandling.Test/Asynchronous.Result.Tests/FlatmapTestsSameTerror.cs
+++ test/Lemonad.ErrorHandling.Test/Asynchronous.Result.Tests/FlatmapTestsSameTerror.cs
@@ -10,7 +10,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var flatSelectorExecuted = false;
             var result = await DivisionAsync(2, 0).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 0);
+                return DivisionAsync(x, 0);
             });
 
             Assert.False(flatSelectorExecuted,
@@ -26,7 +26,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var flatSelectorExecuted = false;
             var result = await DivisionAsync(2, 0).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 2);
+                return DivisionAsync(x, 2);
             });
 
             Assert.False(flatSelectorExecuted,
@@ -43,7 +43,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var resultSelectorExectued = false;
             var result = await DivisionAsync(2, 0).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 0);
+                return DivisionAsync(x, 0);
             }, (y, x) => {
                 resultSelectorExectued = true;
                 return y + x;
@@ -65,7 +65,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var resultSelectorExectued = false;
             var result = await DivisionAsync(2, 0).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 2);
+                return DivisionAsync(x, 2);
             }, (y, x) => {
                 resultSelectorExectued = true;
                 return y + x;
@@ -86,7 +86,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var resultSelectorExectued = false;
             var flatMap = DivisionAsync(2, 2).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 0);
+                return DivisionAsync(x, 0);
             });
 
             Assert.False(flatSelectorExecuted, "Should not get exectued before the value is awaited.");
@@ -108,7 +108,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var errorSelectorExecuted = false;
             var flatMap = DivisionAsync(2, 2).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 2);
+                return DivisionAsync(x, 2);
             });
             Assert.False(flatSelectorExecuted, "Should not get exectued before the value is awaited.");
             var result = await flatMap;
@@ -126,7 +126,7 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
             var resultSelectorExectued = false;
             var flatMap = DivisionAsync(2, 2).FlatMap(x => {
                 flatSelectorExecuted = true;
-                return Division(x, 0);
+                return DivisionAsync(x, 0);
             }, (y, x) => {
                 resultSelectorExectued = true;
                 return y + x;
@@ -150,6 +150,172 @@ namespace Lemonad.ErrorHandling.Test.Asynchronous.Result.Tests {
 
         [Fact]
         public async Task Result_With_Value_FlatmapsRS_Result_with_Value__Expects_Result_With_Value() {
+            var flatSelectorExecuted = false;
+            var resultSelectorExectued = false;
+            var flatMap = DivisionAsync(2, 2).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return DivisionAsync(x, 2);
+            }, (y, x) => {
+                resultSelectorExectued = true;
+                return y + x;
+            });
+            Assert.False(flatSelectorExecuted, "Should not get exectued before the value is awaited.");
+            Assert.False(resultSelectorExectued, "Should not get exectued before the value is awaited.");
+            var result = await flatMap;
+            Assert.True(flatSelectorExecuted, "Flatmapselecotr should get executed.");
+            Assert.True(resultSelectorExectued,
+                "ResultSelector should get executed since both source and the result from flatmapselector contains values.");
+            Assert.True(result.HasValue, "Result should have a value.");
+            Assert.False(result.HasError, "Result should not have a error.");
+            Assert.Equal(1.5d, result.Value);
+            Assert.Equal(default, result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Error_Flatmaps_Result_with_Error__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var result = await DivisionAsync(2, 0).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 0);
+            });
+
+            Assert.False(flatSelectorExecuted,
+                "The flatmap selector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Error_Flatmaps_Result_with_Value__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var result = await DivisionAsync(2, 0).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 2);
+            });
+
+            Assert.False(flatSelectorExecuted,
+                "The flatmap selector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Error_FlatmapsRS_Result_with_Error__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var resultSelectorExectued = false;
+            var result = await DivisionAsync(2, 0).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 0);
+            }, (y, x) => {
+                resultSelectorExectued = true;
+                return y + x;
+            });
+
+            Assert.False(flatSelectorExecuted,
+                "The flatmapSelector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(resultSelectorExectued,
+                "The resultSelector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Error_FlatmapsRS_Result_with_Value__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var resultSelectorExectued = false;
+            var result = await DivisionAsync(2, 0).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 2);
+            }, (y, x) => {
+                resultSelectorExectued = true;
+                return y + x;
+            });
+            Assert.False(flatSelectorExecuted,
+                "The flatmapSelector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(resultSelectorExectued,
+                "The resultSelector should not get exectued if the source Result<T, TError> contains error.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '2' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Value_Flatmaps_Result_with_Error__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var resultSelectorExectued = false;
+            var flatMap = DivisionAsync(2, 2).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 0);
+            });
+
+            Assert.False(flatSelectorExecuted, "Should not get exectued before the value is awaited.");
+            var result = await flatMap;
+
+            Assert.True(flatSelectorExecuted,
+                "The flatmapSelector should get exectued.");
+            Assert.False(resultSelectorExectued,
+                "The resultSelector should not get executed since flatselector result failed.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Value_Flatmaps_Result_with_Value__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var errorSelectorExecuted = false;
+            var flatMap = DivisionAsync(2, 2).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 2);
+            });
+            Assert.False(flatSelectorExecuted, "Should not get exectued before the value is awaited.");
+            var result = await flatMap;
+            Assert.True(flatSelectorExecuted, "flatmapselector should get executed.");
+            Assert.False(errorSelectorExecuted, "Errorselector should not get exeuted.");
+            Assert.True(result.HasValue, "Result should have a value.");
+            Assert.False(result.HasError, "Result should not have a error.");
+            Assert.Equal(0.5d, result.Value);
+            Assert.Equal(default, result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Value_FlatmapsRS_Result_with_Error__Expects_Result_With_Value_Sync() {
+            var flatSelectorExecuted = false;
+            var resultSelectorExectued = false;
+            var flatMap = DivisionAsync(2, 2).FlatMap(x => {
+                flatSelectorExecuted = true;
+                return Division(x, 0);
+            }, (y, x) => {
+                resultSelectorExectued = true;
+                return y + x;
+            });
+            Assert.False(flatSelectorExecuted, "Should not get exectued before the value is awaited.");
+            var result = await flatMap;
+
+            Assert.True(flatSelectorExecuted,
+                "The flatmapSelector should get exectued.");
+            Assert.False(resultSelectorExectued,
+                "The resultSelector should not get executed since flatselector result failed.");
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+            Assert.False(result.HasValue, "Result should not have a value.");
+            Assert.True(result.HasError, "Result should have a error.");
+            Assert.Equal(default, result.Value);
+            Assert.Equal("Can not divide '1' with '0'.", result.Error);
+        }
+
+        [Fact]
+        public async Task Result_With_Value_FlatmapsRS_Result_with_Value__Expects_Result_With_Value_Sync() {
             var flatSelectorExecuted = false;
             var resultSelectorExectued = false;
             var flatMap = DivisionAsync(2, 2).FlatMap(x => {
