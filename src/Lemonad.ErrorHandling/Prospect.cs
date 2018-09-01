e12180718c43cda6ba45d36e132d4e7aa33f2565
Robert Andersson <robert.andersson.swe@gmail.com>
2018-09-01 21:55:45 +0200

    Add Prospect<T> class

---
+++ src/Lemonad.ErrorHandling/Prospect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+
+namespace Lemonad.ErrorHandling {
+    public class Prospect<T> {
+        private readonly Outcome<T, Unit> _outcome;
+
+        public Prospect(Outcome<T, Unit> outcome) => _outcome = outcome;
+
+        public Task<bool> HasValue => _outcome.HasValue;
+
+        [Pure]
+        public Prospect<TResult> Map<TResult>(Func<T, TResult> selector) =>
+            new Prospect<TResult>(_outcome.Map(selector));
+    }
+}
