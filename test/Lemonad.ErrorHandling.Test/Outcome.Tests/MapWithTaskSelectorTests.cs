ae10013567fbee0e827b5e0b6227d9f502b2c825
Robert Andersson <robert.andersson.swe@gmail.com>
2018-09-02 16:05:33 +0200

    Add tests for task map

---
+++ test/Lemonad.ErrorHandling.Test/Outcome.Tests/MapWithTaskSelectorTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Lemonad.ErrorHandling.Extensions;
+using Xunit;
+
+namespace Lemonad.ErrorHandling.Test.Outcome.Tests {
+    public class MapWithTaskSelectorTests {
+        [Fact]
+        public async Task Result_With_Error_Maps__Expects_Selector_Never_Be_Executed() {
+            var selectorExectued = false;
+            var division = await AssertionUtilities
+                .DivisionAsync(2, 0)
+                .AsOutcome()
+                .Map(async x => {
+                    selectorExectued = true;
+                    await Task.Delay(200);
+                    return x * 8;
+                }).Match(d => d, s => -1);
+
+            Assert.False(selectorExectued,
+                "The selector function should never get executed if there's no value in the Result<T, TError>.");
+            Assert.Equal(-1, division);
+        }
+
+        [Fact]
+        public async Task Result_With_Value_Maps__Expects_Selector_Be_Executed_And_Value_To_Be_Mapped() {
+            var selectorExectued = false;
+            var division = await AssertionUtilities
+                .DivisionAsync(10, 2)
+                .AsOutcome()
+                .Map(async x => {
+                        selectorExectued = true;
+                        await Task.Delay(200);
+                        return x * 4;
+                    }
+                ).Match(x => x, x => -1);
+            Assert.True(selectorExectued, "The selector function should get executed since the result has value.");
+            Assert.Equal(20d, division);
+        }
+    }
+}
