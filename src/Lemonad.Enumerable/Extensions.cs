7b14f3f1e84bd592744022bdf0aed4e63ba2d79c
Robert Andersson <robert.andersson.swe@gmail.com>
2018-09-16 23:49:47 +0200

    Add enumerable extension assembly

---
+++ src/Lemonad.Enumerable/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lemonad.Enumerable {
+    public static class Extensions {
+        public static IEnumerable<IChunk<T>> ChunkBy<T>(this IEnumerable<T> source, int count) {
+            return source
+                .Select((x, y) => (Element: x, Index: y))
+                .GroupBy<(T Element, int Index), int, IChunk<T>>(
+                    x => x.Index / count,
+                    (_, y) => new Chunk<T>(y.Select(x => x.Element), count)
+                );
+        }
+
+        public interface IChunk<out T> : IEnumerable<T> {
+            int MaxCount { get; }
+        }
+
+        private readonly struct Chunk<T> : IChunk<T> {
+            private readonly IEnumerable<T> _chunk;
+
+            public Chunk(IEnumerable<T> chunk, int count) {
+                _chunk = chunk;
+                MaxCount = count;
+            }
+
+            public IEnumerator<T> GetEnumerator() => _chunk.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            public int MaxCount { get; }
+        }
+    }
+}
