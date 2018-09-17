ec5b42efe31adb45946ac13280cd1a63d4e9e79f
Robert Andersson <robert.andersson.swe@gmail.com>
2018-09-17 20:10:19 +0200

    Add tests

--- src/Lemonad.Enumerable/Extensions.cs
+++ src/Lemonad.Enumerable/Extensions.cs
@@ -1,26 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Lemonad.Enumerable {
     public static class Extensions {
-        public static IEnumerable<IChunk<T>> ChunkBy<T>(this IEnumerable<T> source, int count) {
+        /// <summary>
+        /// Chunks the elements of a sequence according to a amount and creates a result value from each group and its max capacity.
+        /// </summary>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> whose elements to chunk.</param>
+        /// <param name="count">The max capacity for each <see cref="IChunk{T}"/>.</param>
+        /// <typeparam name="TSource">The type of the elements in <paramref name="source"/>.</typeparam>
+        /// <returns>The type of the elements <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentException">When <paramref name="count"/> is below 1.</exception>
+        public static IEnumerable<IChunk<TSource>> ChunkBy<TSource>(this IEnumerable<TSource> source, int count) {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (count < 1) throw new ArgumentException($"Parameter: '{nameof(count)}' ({count}) can not be below 1.");
             return source
                 .Select((x, y) => (Element: x, Index: y))
-                .GroupBy<(T Element, int Index), int, IChunk<T>>(
+                .GroupBy<(TSource Element, int Index), int, IChunk<TSource>>(
                     x => x.Index / count,
-                    (_, y) => new Chunk<T>(y.Select(x => x.Element), count)
+                    (_, y) => new Chunk<TSource>(y.Select(x => x.Element), count)
                 );
-        }
-
-        public interface IChunk<out T> : IEnumerable<T> {
-            int MaxCount { get; }
         }
 
         private readonly struct Chunk<T> : IChunk<T> {
             private readonly IEnumerable<T> _chunk;
 
-            public Chunk(IEnumerable<T> chunk, int count) {
+            public Chunk(in IEnumerable<T> chunk, int count) {
                 _chunk = chunk;
                 MaxCount = count;
             }
@@ -29,7 +37,18 @@ namespace Lemonad.Enumerable {
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+            /// <summary>
+            /// The max amount of allowed elements in <see cref="IChunk{T}"/>.
+            /// </summary>
             public int MaxCount { get; }
         }
+    }
+
+    /// <summary>
+    /// Represents a collection of elements that have a common max count.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements.</typeparam>
+    public interface IChunk<out T> : IEnumerable<T> {
+        int MaxCount { get; }
     }
 }
---
+++ test/Lemonad.Enumerable.Tests/ChunkByTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Lemonad.Enumerable.Tests {
+    public class ChunkByTests {
+        [Fact]
+        public void Chunk_Enumerable_Of_10_Into_Four_Chunks() {
+            const int chunkSize = 3;
+            var chunks = System.Linq.Enumerable.Range(0, 10).ChunkBy(chunkSize).Select(x => x).ToArray();
+            // Verify all chunks have equal max size.
+            Assert.All(chunks, x => Assert.Equal(chunkSize, x.MaxCount));
+            var arr = chunks.Select(x => x.ToArray()).ToArray();
+
+            Assert.Equal(4, arr.Length);
+
+            // First chunk
+            Assert.Equal(0, arr[0][0]);
+            Assert.Equal(1, arr[0][1]);
+            Assert.Equal(2, arr[0][2]);
+
+            // Second chunk
+            Assert.Equal(3, arr[1][0]);
+            Assert.Equal(4, arr[1][1]);
+            Assert.Equal(5, arr[1][2]);
+
+            // Third chunk
+            Assert.Equal(6, arr[2][0]);
+            Assert.Equal(7, arr[2][1]);
+            Assert.Equal(8, arr[2][2]);
+
+            // Fourth chunk
+            Assert.Equal(9, arr[3][0]);
+        }
+
+        [Fact]
+        public void Chunk_Enumerable_Of_10_With_Negative_Integer_Throws() => Assert.Throws<ArgumentException>(
+            () => System.Linq.Enumerable.Range(0, 10).ChunkBy(-1).Select(x => x.ToArray()).ToArray());
+
+        [Fact]
+        public void Chunk_Null_Enumerable_Throws() {
+            IEnumerable<int> enumerable = null;
+            Assert.Throws<ArgumentNullException>("source", () => enumerable.ChunkBy(4));
+        }
+    }
+}
