c7c68e0cf8e9ae4f5acab1147b86f34d3dfcae94
Robert Andersson <robert.andersson.swe@gmail.com>
2018-09-23 15:49:41 +0200

    Add docs for Value and Error

--- src/Lemonad.ErrorHandling/Either.cs
+++ src/Lemonad.ErrorHandling/Either.cs
@@ -21,7 +21,38 @@ namespace Lemonad.ErrorHandling {
         /// </summary>
         public bool HasError { get; }
 
+        /// <summary>
+        /// The potential <typeparamref name="TError"/>.
+        /// </summary>
+        /// <remarks>
+        /// Verify with <see cref="HasError"/> before using this, unless your certain that the error exists.
+        /// </remarks>
+        /// <example>
+        /// <code language="c#">
+        /// if (Either.HasError)
+        /// {
+        ///     // Safe to use.
+        ///     Console.WriteLine(Either.Error)
+        /// }
+        /// </code>
+        /// </example>
         public TError Error { get; }
+
+        /// <summary>
+        /// The potential <typeparamref name="T"/>.
+        /// </summary>
+        /// <remarks>
+        /// Verify with <see cref="HasValue"/> before using this, unless your certain that the value exists.
+        /// </remarks>
+        /// <example>
+        /// <code language="c#">
+        /// if (Either.HasValue)
+        /// {
+        ///     // Safe to use.
+        ///     Console.WriteLine(Either.Value)
+        /// }
+        /// </code>
+        /// </example>
         public T Value { get; }
 
         public Either(in T value, in TError error, bool hasError, bool hasValue) {
