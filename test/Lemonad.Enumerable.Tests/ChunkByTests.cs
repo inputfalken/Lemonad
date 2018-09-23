f2ee4824d3bd95a2bf6a300590cdfa3caea8c48e
Robert Andersson <robert.andersson.swe@gmail.com>
2018-09-23 19:53:15 +0200

    Update chunkby implementation

--- test/Lemonad.Enumerable.Tests/ChunkByTests.cs
+++ test/Lemonad.Enumerable.Tests/ChunkByTests.cs
@@ -1,45 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace Lemonad.Enumerable.Tests {
-    public class ChunkByTests {
+    public class ChunkBy {
         [Fact]
-        public void Chunk_Enumerable_Of_10_Into_Four_Chunks() {
+        public void Transform_Enumerable_Of_10_Into_Chunks_With_Size_3() {
             const int chunkSize = 3;
-            var chunks = System.Linq.Enumerable.Range(0, 10).ChunkBy(chunkSize).Select(x => x).ToArray();
-            // Verify all chunks have equal max size.
-            Assert.All(chunks, x => Assert.Equal(chunkSize, x.MaxCount));
-            var arr = chunks.Select(x => x.ToArray()).ToArray();
+            var arr = System.Linq.Enumerable.Range(0, 10).ChunkBy(chunkSize);
 
-            Assert.Equal(4, arr.Length);
-
-            // First chunk
-            Assert.Equal(0, arr[0][0]);
-            Assert.Equal(1, arr[0][1]);
-            Assert.Equal(2, arr[0][2]);
-
-            // Second chunk
-            Assert.Equal(3, arr[1][0]);
-            Assert.Equal(4, arr[1][1]);
-            Assert.Equal(5, arr[1][2]);
-
-            // Third chunk
-            Assert.Equal(6, arr[2][0]);
-            Assert.Equal(7, arr[2][1]);
-            Assert.Equal(8, arr[2][2]);
-
-            // Fourth chunk
-            Assert.Equal(9, arr[3][0]);
+            IEnumerable<IEnumerable<int>> expected = new[] {
+                new[] {0, 1, 2},
+                new[] {3, 4, 5},
+                new[] {6, 7, 8},
+                new[] {9},
+            };
+            Assert.Equal(expected, arr);
         }
 
         [Fact]
-        public void Chunk_Enumerable_Of_10_With_Negative_Integer_Throws() => Assert.Throws<ArgumentException>(
-            () => System.Linq.Enumerable.Range(0, 10).ChunkBy(-1).Select(x => x.ToArray()).ToArray());
+        public void Transform_Enumerable_Of_10_Into_Chunks_With_Size_10() {
+            const int chunkSize = 10;
+            var arr = System.Linq.Enumerable.Range(0, 100).ChunkBy(chunkSize);
+
+            IEnumerable<int>[] expected = {
+                System.Linq.Enumerable.Range(0, 10),
+                System.Linq.Enumerable.Range(10, 10),
+                System.Linq.Enumerable.Range(20, 10),
+                System.Linq.Enumerable.Range(30, 10),
+                System.Linq.Enumerable.Range(40, 10),
+                System.Linq.Enumerable.Range(50, 10),
+                System.Linq.Enumerable.Range(60, 10),
+                System.Linq.Enumerable.Range(70, 10),
+                System.Linq.Enumerable.Range(80, 10),
+                System.Linq.Enumerable.Range(90, 10),
+            };
+            Assert.Equal(expected, arr);
+        }
 
         [Fact]
-        public void Chunk_Null_Enumerable_Throws() {
+        public void Transform_Enumerable_Of_100_Into_Chunks_With_Size_Eleven() {
+            const int chunkSize = 11;
+            var arr = System.Linq.Enumerable.Range(0, 100).ChunkBy(chunkSize);
+
+            IEnumerable<int>[] expected = {
+                System.Linq.Enumerable.Range(0, 11),
+                System.Linq.Enumerable.Range(11, 11),
+                System.Linq.Enumerable.Range(22, 11),
+                System.Linq.Enumerable.Range(33, 11),
+                System.Linq.Enumerable.Range(44, 11),
+                System.Linq.Enumerable.Range(55, 11),
+                System.Linq.Enumerable.Range(66, 11),
+                System.Linq.Enumerable.Range(77, 11),
+                System.Linq.Enumerable.Range(88, 11),
+                System.Linq.Enumerable.Range(99, 1),
+            };
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void Supplying_Negative_Size_Throws_ArgumentException() => Assert.Throws<ArgumentException>(
+            () => System.Linq.Enumerable.Range(0, 10).ChunkBy(-1));
+
+        [Fact]
+        public void Supplying_Null_Enumerable_Throws_ArgumentNullException() {
             IEnumerable<int> enumerable = null;
             Assert.Throws<ArgumentNullException>("source", () => enumerable.ChunkBy(4));
         }
