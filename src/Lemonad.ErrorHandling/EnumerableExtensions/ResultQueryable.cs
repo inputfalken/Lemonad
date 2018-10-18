3dda37eb9274ba47ca446c8a3ed3c50b93c326aa
Robert Andersson <robert.andersson.swe@gmail.com>
2018-10-19 00:07:37 +0200

    Add extensions to IQueryable

---
+++ src/Lemonad.ErrorHandling/EnumerableExtensions/ResultQueryable.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Lemonad.ErrorHandling.EnumerableExtensions {
+    public static class ResultQueryable {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> FirstOrError<TSource, TError>(this IQueryable<TSource> source,
+            Func<TError> errorSelector) where TSource : class =>
+            source.FirstOrDefault().ToResult(x => x != null, _ => errorSelector());
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> SingleOrError<TSource, TError>(this IQueryable<TSource> source,
+            Func<TError> errorSelector) where TSource : class =>
+            source.SingleOrDefault().ToResult(x => x != null, _ => errorSelector());
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> FirstOrError<TSource, TError>(this IQueryable<TSource> source,
+            Expression<Func<TSource, bool>> predicate, Func<TError> errorSelector) where TSource : class =>
+            source.FirstOrDefault(predicate).ToResult(x => x != null, _ => errorSelector());
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> SingleOrError<TSource, TError>(this IQueryable<TSource> source,
+            Expression<Func<TSource, bool>> predicate, Func<TError> errorSelector) where TSource : class =>
+            source.SingleOrDefault(predicate).ToResult(x => x != null, _ => errorSelector());
+    }
+}
