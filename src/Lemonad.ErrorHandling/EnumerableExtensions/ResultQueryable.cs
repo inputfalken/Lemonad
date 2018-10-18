4d02eea23bb21d7f7853d1de0440dec1b0b94456
Robert Andersson <robert.andersson.swe@gmail.com>
2018-10-19 00:10:28 +0200

    Add overloads for nullable structs

--- src/Lemonad.ErrorHandling/EnumerableExtensions/ResultQueryable.cs
+++ src/Lemonad.ErrorHandling/EnumerableExtensions/ResultQueryable.cs
@@ -53,5 +53,55 @@ namespace Lemonad.ErrorHandling.EnumerableExtensions {
         public static IResult<TSource, TError> SingleOrError<TSource, TError>(this IQueryable<TSource> source,
             Expression<Func<TSource, bool>> predicate, Func<TError> errorSelector) where TSource : class =>
             source.SingleOrDefault(predicate).ToResult(x => x != null, _ => errorSelector());
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> FirstOrError<TSource, TError>(this IQueryable<TSource?> source,
+            Func<TError> errorSelector) where TSource : struct =>
+            source.FirstOrDefault().ToResult(x => x.HasValue, _ => errorSelector()).Map(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> SingleOrError<TSource, TError>(this IQueryable<TSource?> source,
+            Func<TError> errorSelector) where TSource : struct =>
+            source.SingleOrDefault().ToResult(x => x.HasValue, _ => errorSelector()).Map(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> FirstOrError<TSource, TError>(this IQueryable<TSource?> source,
+            Expression<Func<TSource, bool>> predicate, Func<TError> errorSelector) where TSource : struct =>
+            source.FirstOrDefault(predicate).ToResult(x => x.HasValue, _ => errorSelector()).Map(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="errorSelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <returns></returns>
+        public static IResult<TSource, TError> SingleOrError<TSource, TError>(this IQueryable<TSource?> source,
+            Expression<Func<TSource, bool>> predicate, Func<TError> errorSelector) where TSource : struct =>
+            source.SingleOrDefault(predicate).ToResult(x => x.HasValue, _ => errorSelector()).Map(x => x.Value);
     }
 }
