1389dcc1023aba4ad9c174ba6835976e559165ec
Robert Andersson <robert.andersson.swe@gmail.com>
2018-11-01 00:34:42 +0100

    Avoid awaiting multiple times

--- src/Lemonad.ErrorHandling/EitherAsync.cs
+++ src/Lemonad.ErrorHandling/EitherAsync.cs
@@ -1,29 +1,47 @@
 ﻿using System.Threading.Tasks;
 
 namespace Lemonad.ErrorHandling {
-    // Can this be immutable?
     internal class EitherAsync<T, TError> : IEitherAsync<T, TError> {
         private readonly Task<IEither<T, TError>> _either;
         public Task<bool> HasValue => ResolveValue();
         public Task<bool> HasError => ResolveError();
+        private bool _hasValue;
+        private bool _hasError;
+        private bool _isAwaited;
 
         public EitherAsync(Task<IEither<T, TError>> either) => _either = either;
 
         private async Task<bool> ResolveValue() {
+            // Avoid redundant awaits
+            if (_isAwaited) return _hasValue;
             var either = await _either.ConfigureAwait(false);
-            if (either.HasValue)
+            _isAwaited = true;
+            if (either.HasValue) {
+                _hasValue = true;
                 Value = either.Value;
-            else
+            }
+            else {
+                _hasError = true;
                 Error = either.Error;
+            }
+
             return either.HasValue;
         }
 
         private async Task<bool> ResolveError() {
+            // Avoid redundant awaits
+            if (_isAwaited) return _hasError;
             var either = await _either.ConfigureAwait(false);
-            if (either.HasError)
+            _isAwaited = true;
+            if (either.HasError) {
+                _hasError = true;
                 Error = either.Error;
-            else
+            }
+            else {
+                _hasValue = true;
                 Value = either.Value;
+            }
+
             return either.HasError;
         }
 
