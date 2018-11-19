ee117e09973b7b43a10f0be2013dea338f6fb693
Robert Andersson <robert.andersson.swe@gmail.com>
2018-11-19 01:25:49 +0100

    Add back _isAwaited
    The worst case for this data structure is that the members get
    assigned multiple times

--- src/Lemonad.ErrorHandling/Internal/Either/AsyncEither.cs
+++ src/Lemonad.ErrorHandling/Internal/Either/AsyncEither.cs
@@ -4,8 +4,9 @@ using Lemonad.ErrorHandling.Exceptions;
 namespace Lemonad.ErrorHandling.Internal.Either {
     internal class AsyncEither<T, TError> : IAsyncEither<T, TError> {
         private readonly Task<IEither<T, TError>> _either;
-        private bool? _hasError;
-        private bool? _hasValue;
+        private bool _hasError;
+        private bool _hasValue;
+        private bool _isAwaited;
         private T _value;
         private TError _error;
 
@@ -17,7 +18,7 @@ namespace Lemonad.ErrorHandling.Internal.Either {
 
         /// Should this throw exception when <see cref="IAsyncEither{T,TError}.HasValue"/> is true?
         public TError Error {
-            get => _hasError.HasValue
+            get => _isAwaited
                 ? _error
                 : throw new InvalidEitherStateException(
                     $"Can not access property '{nameof(IAsyncEither<T, TError>.Error)}' of '{nameof(IAsyncEither<T, TError>)}', before property '{nameof(IAsyncEither<T, TError>.HasError)}' or '{nameof(IAsyncEither<T, TError>.HasError)}' has been awaited."
@@ -28,7 +29,7 @@ namespace Lemonad.ErrorHandling.Internal.Either {
 
         /// Should this throw exception when <see cref="IAsyncEither{T,TError}.HasError"/> is true?
         public T Value {
-            get => _hasValue.HasValue
+            get => _isAwaited
                 ? _value
                 : throw new InvalidEitherStateException(
                     $"Can not access property '{nameof(IAsyncEither<T, TError>.Value)}' of '{nameof(IAsyncEither<T, TError>)}', before property '{nameof(IAsyncEither<T, TError>.HasError)}' or '{nameof(IAsyncEither<T, TError>.HasError)}' has been awaited."
@@ -38,12 +39,13 @@ namespace Lemonad.ErrorHandling.Internal.Either {
 
         private async Task<bool> Resolve(bool returnHasValue) {
             var either = await _either.ConfigureAwait(false);
+            _isAwaited = true;
             _hasError = either.HasError;
             _hasValue = either.HasValue;
             if (either.HasValue) Value = either.Value;
             else Error = either.Error;
 
-            return returnHasValue ? _hasValue.Value : _hasError.Value;
+            return returnHasValue ? _hasValue : _hasError;
         }
     }
 }
