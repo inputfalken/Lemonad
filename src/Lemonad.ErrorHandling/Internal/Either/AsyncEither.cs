ef15ff468ef294467f02b13267a77ea6b8f48254
Robert Andersson <robert.andersson.swe@gmail.com>
2018-11-20 00:27:55 +0100

    Ensure that assignments only happen once

--- src/Lemonad.ErrorHandling/Internal/Either/AsyncEither.cs
+++ src/Lemonad.ErrorHandling/Internal/Either/AsyncEither.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Lemonad.ErrorHandling.Exceptions;
 
 namespace Lemonad.ErrorHandling.Internal.Either {
@@ -6,19 +8,23 @@ namespace Lemonad.ErrorHandling.Internal.Either {
         private readonly Task<IEither<T, TError>> _either;
         private bool _hasError;
         private bool _hasValue;
-        private bool _hasBeenAssigned;
+        private bool _isAssigned;
         private T _value;
         private TError _error;
+        private readonly SemaphoreSlim _semaphoreSlim;
 
-        public AsyncEither(Task<IEither<T, TError>> either) => _either = either;
+        public AsyncEither(Task<IEither<T, TError>> either) {
+            _either = either;
+            _semaphoreSlim = new SemaphoreSlim(1, 1);
+        }
 
-        public Task<bool> HasError => Resolve(false);
+        public Task<bool> HasError => _isAssigned ? Task.FromResult(_hasValue) :Resolve(false);
 
-        public Task<bool> HasValue => Resolve(true);
+        public Task<bool> HasValue => _isAssigned ? Task.FromResult(_hasValue) : Resolve(true);
 
         /// Should this throw exception when <see cref="IAsyncEither{T,TError}.HasValue"/> is true?
         public TError Error {
-            get => _hasBeenAssigned
+            get => _isAssigned
                 ? _error
                 : throw new InvalidEitherStateException(
                     $"Can not access property '{nameof(IAsyncEither<T, TError>.Error)}' of '{nameof(IAsyncEither<T, TError>)}', before property '{nameof(IAsyncEither<T, TError>.HasError)}' or '{nameof(IAsyncEither<T, TError>.HasError)}' has been awaited."
@@ -29,7 +35,7 @@ namespace Lemonad.ErrorHandling.Internal.Either {
 
         /// Should this throw exception when <see cref="IAsyncEither{T,TError}.HasError"/> is true?
         public T Value {
-            get => _hasBeenAssigned
+            get => _isAssigned
                 ? _value
                 : throw new InvalidEitherStateException(
                     $"Can not access property '{nameof(IAsyncEither<T, TError>.Value)}' of '{nameof(IAsyncEither<T, TError>)}', before property '{nameof(IAsyncEither<T, TError>.HasError)}' or '{nameof(IAsyncEither<T, TError>.HasError)}' has been awaited."
@@ -37,17 +43,27 @@ namespace Lemonad.ErrorHandling.Internal.Either {
             private set => _value = value;
         }
 
-        /// Worst case scenario,  <see cref="Value"/> or <see cref="Error"/> gets assigned multiple times.
         private async Task<bool> Resolve(bool returnHasValue) {
+            await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
+            if (_isAssigned) return returnHasValue ? _hasValue : _hasError;
+            try {
+                await AssignProperties();
+            }
+            finally {
+                _semaphoreSlim.Release();
+            }
+
+            return returnHasValue ? _hasValue : _hasError;
+        }
+
+        private async Task AssignProperties() {
             var either = await _either.ConfigureAwait(false);
             if (either.HasValue) Value = either.Value;
             else Error = either.Error;
 
             _hasError = either.HasError;
             _hasValue = either.HasValue;
-            _hasBeenAssigned = true;
-
-            return returnHasValue ? _hasValue : _hasError;
+            _isAssigned = true;
         }
     }
 }
