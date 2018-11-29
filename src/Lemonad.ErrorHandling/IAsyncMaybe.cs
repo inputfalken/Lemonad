6a0130fbb964945113f18ca0a816b3de86a63fba
Robert Andersson <robert.andersson.swe@gmail.com>
2018-11-29 21:40:38 +0100

    Add base of interface

---
+++ src/Lemonad.ErrorHandling/IAsyncMaybe.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Lemonad.ErrorHandling.Internal;
+
+namespace Lemonad.ErrorHandling {
+    public interface IAsyncMaybe<out T> {
+        /// <summary>
+        ///     Gets a value indicating whether the current <see cref="Maybe{T}" /> object has a valid value of
+        ///     its underlying type.
+        /// </summary>
+        /// <returns>
+        ///     true if the current <see cref="Maybe{T}"></see> object has a value; false if the current
+        ///     <see cref="Maybe{T}"></see> object has no value.
+        /// </returns>
+        Task<bool> HasValue { get; }
+
+        /// <summary>
+        ///     Gets the value of the current <see cref="Maybe{T}"></see> object if <see cref="HasValue" /> is true.
+        /// </summary>
+        /// <example>
+        ///     <code language="c#">
+        ///  if (Either.HasValue)
+        ///  {
+        ///      // Safe to use.
+        ///      Console.WriteLine(Either.Value)
+        ///  }
+        ///  </code>
+        /// </example>
+        T Value { get; }
+    }
+}
