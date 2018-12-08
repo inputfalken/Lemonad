a110ff86bc81819381ee176aaf3cc0f5b9a0729d
Robert Andersson <robert.andersson.swe@gmail.com>
2018-12-08 15:54:28 +0100

    Add none async members

--- src/Lemonad.ErrorHandling/IAsyncMaybe.cs
+++ src/Lemonad.ErrorHandling/IAsyncMaybe.cs
@@ -8,18 +8,23 @@ namespace Lemonad.ErrorHandling {
         IAsyncMaybe<T> Do(Action action);
         IAsyncMaybe<T> DoWith(Action<T> someAction);
         IAsyncMaybe<T> Filter(Func<T, bool> predicate);
-        IAsyncMaybe<TResult> FlatMap<TResult>(Func<T, IAsyncMaybe<TResult>> flatMapSelector);
+        IAsyncMaybe<T> Flatten<TResult>(Func<T, IMaybe<TResult>> selector);
+        IAsyncMaybe<T> FlattenAsync<TResult>(Func<T, IAsyncMaybe<TResult>> selector);
+        IAsyncMaybe<T> IsNoneWhen(Func<T, bool> predicate);
+        IAsyncMaybe<TResult> FlatMap<TResult>(Func<T, IMaybe<TResult>> flatMapSelector);
+        IAsyncMaybe<TResult> FlatMapAsync<TResult>(Func<T, IAsyncMaybe<TResult>> flatMapSelector);
+        IAsyncMaybe<TResult> Map<TResult>(Func<T, TResult> selector);
+        Task Match(Action<T> someAction, Action noneAction);
+        TResult Match<TResult>(Func<T, TResult> someSelector, Func<TResult> noneSelector);
 
-        IAsyncMaybe<TResult> FlatMap<TFlatMap, TResult>(
+        IAsyncMaybe<TResult> FlatMapAsync<TFlatMap, TResult>(
             Func<T, IAsyncMaybe<TFlatMap>> flatMapSelector,
             Func<T, TFlatMap, TResult> resultSelector
         );
 
-        IAsyncMaybe<TResult> FlatMap<TResult>(Func<T, TResult?> flatSelector) where TResult : struct;
-        IAsyncMaybe<T> Flatten<TResult>(Func<T, IAsyncMaybe<TResult>> selector);
-        IAsyncMaybe<T> IsNoneWhen(Func<T, bool> predicate);
-        IAsyncMaybe<TResult> Map<TResult>(Func<T, TResult> selector);
-        Task Match(Action<T> someAction, Action noneAction);
-        TResult Match<TResult>(Func<T, TResult> someSelector, Func<TResult> noneSelector);
+        IAsyncMaybe<TResult> FlatMap<TFlatMap, TResult>(
+            Func<T, IMaybe<TFlatMap>> flatMapSelector,
+            Func<T, TFlatMap, TResult> resultSelector
+        );
     }
 }
--- src/Lemonad.ErrorHandling/Internal/AsyncMaybe.cs
+++ src/Lemonad.ErrorHandling/Internal/AsyncMaybe.cs
@@ -13,13 +13,23 @@ namespace Lemonad.ErrorHandling.Internal {
 
         public IAsyncMaybe<T> Filter(Func<T, bool> predicate) => throw new NotImplementedException();
 
-        public IAsyncMaybe<TResult> FlatMap<TResult>(Func<T, IAsyncMaybe<TResult>> flatMapSelector) => throw new NotImplementedException();
+        public IAsyncMaybe<TResult> FlatMapAsync<TResult>(Func<T, IAsyncMaybe<TResult>> flatMapSelector) =>
+            throw new NotImplementedException();
 
-        public IAsyncMaybe<TResult> FlatMap<TFlatMap, TResult>(Func<T, IAsyncMaybe<TFlatMap>> flatMapSelector, Func<T, TFlatMap, TResult> resultSelector) => throw new NotImplementedException();
+        public IAsyncMaybe<TResult> FlatMapAsync<TFlatMap, TResult>(Func<T, IAsyncMaybe<TFlatMap>> flatMapSelector,
+            Func<T, TFlatMap, TResult> resultSelector) => throw new NotImplementedException();
 
-        public IAsyncMaybe<TResult> FlatMap<TResult>(Func<T, TResult?> flatSelector) where TResult : struct => throw new NotImplementedException();
+        public IAsyncMaybe<T> FlattenAsync<TResult>(Func<T, IAsyncMaybe<TResult>> selector) =>
+            throw new NotImplementedException();
 
-        public IAsyncMaybe<T> Flatten<TResult>(Func<T, IAsyncMaybe<TResult>> selector) => throw new NotImplementedException();
+        public IAsyncMaybe<TResult> FlatMap<TResult>(Func<T, IMaybe<TResult>> flatMapSelector) =>
+            throw new NotImplementedException();
+
+        public IAsyncMaybe<TResult> FlatMap<TFlatMap, TResult>(Func<T, IMaybe<TFlatMap>> flatMapSelector,
+            Func<T, TFlatMap, TResult> resultSelector) => throw new NotImplementedException();
+
+        public IAsyncMaybe<T> Flatten<TResult>(Func<T, IMaybe<TResult>> selector) =>
+            throw new NotImplementedException();
 
         public IAsyncMaybe<T> IsNoneWhen(Func<T, bool> predicate) => throw new NotImplementedException();
 
@@ -27,6 +37,7 @@ namespace Lemonad.ErrorHandling.Internal {
 
         public Task Match(Action<T> someAction, Action noneAction) => throw new NotImplementedException();
 
-        public TResult Match<TResult>(Func<T, TResult> someSelector, Func<TResult> noneSelector) => throw new NotImplementedException();
+        public TResult Match<TResult>(Func<T, TResult> someSelector, Func<TResult> noneSelector) =>
+            throw new NotImplementedException();
     }
 }
