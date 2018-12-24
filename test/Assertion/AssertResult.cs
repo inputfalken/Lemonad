4b2a5a9998b33c3e706fbbea92883ab08dc0a46a
Robert Andersson <robert.andersson.swe@gmail.com>
2018-12-24 02:57:11 +0100

    Use new assertion extension for async result

--- test/Assertion/AssertResult.cs
+++ test/Assertion/AssertResult.cs
@@ -1,22 +1,54 @@
-﻿using Lemonad.ErrorHandling;
+﻿using System.Threading.Tasks;
+using Lemonad.ErrorHandling;
+using Lemonad.ErrorHandling.Extensions.AsyncResult;
+using Lemonad.ErrorHandling.Extensions.Result.Task;
 using Xunit;
 
 namespace Assertion {
     public static class AssertResult {
-        public static IResult<T, TError> AssertValue<T, TError>(this IResult<T, TError> result, T expected) {
-            Assert.True(result.Either.HasValue);
-            Assert.False(result.Either.HasError);
-            Assert.Equal(default, result.Either.Error);
-            Assert.Equal(expected, result.Either.Value);
-            return result;
+        public static IResult<T, TError> AssertValue<T, TError>(this IResult<T, TError> source, T expected) {
+            Assert.True(source.Either.HasValue);
+            Assert.False(source.Either.HasError);
+            Assert.Equal(default, source.Either.Error);
+            Assert.Equal(expected, source.Either.Value);
+            return source;
         }
 
-        public static IResult<T, TError> AssertError<T, TError>(this IResult<T, TError> result, TError expected) {
-            Assert.False(result.Either.HasValue);
-            Assert.True(result.Either.HasError);
-            Assert.Equal(expected, result.Either.Error);
-            Assert.Equal(default, result.Either.Value);
-            return result;
+        public static IResult<T, TError> AssertError<T, TError>(this IResult<T, TError> source, TError expected) {
+            Assert.False(source.Either.HasValue);
+            Assert.True(source.Either.HasError);
+            Assert.Equal(expected, source.Either.Error);
+            Assert.Equal(default, source.Either.Value);
+            return source;
+        }
+
+        public static IAsyncResult<T, TError> AssertValue<T, TError>(this IAsyncResult<T, TError> source, T expected) {
+            async Task<IResult<T, TError>> Resolve() {
+                var result = await source;
+                Assert.True(result.Either.HasValue);
+                Assert.False(result.Either.HasError);
+                Assert.Equal(default, result.Either.Error);
+                Assert.Equal(expected, result.Either.Value);
+                return result;
+            }
+
+            return Resolve().ToAsyncResult();
+        }
+
+        public static IAsyncResult<T, TError> AssertError<T, TError>(
+            this IAsyncResult<T, TError> source,
+            TError expected
+        ) {
+            async Task<IResult<T, TError>> Resolve() {
+                var result = await source;
+                Assert.False(result.Either.HasValue);
+                Assert.True(result.Either.HasError);
+                Assert.Equal(expected, result.Either.Error);
+                Assert.Equal(default, result.Either.Value);
+                return result;
+            }
+
+            return Resolve().ToAsyncResult();
         }
     }
 }
--- test/Lemonad.ErrorHandling.Unit/AsyncResult.Tests/DoWithErrorTests.cs
+++ test/Lemonad.ErrorHandling.Unit/AsyncResult.Tests/DoWithErrorTests.cs
@@ -9,26 +9,24 @@ namespace Lemonad.ErrorHandling.Unit.AsyncResult.Tests {
         public async Task
             Result_With_Error__Expects_Action_To_Be_Invoked() {
             var actionExectued = false;
-            var result = await AssertionUtilities.DivisionAsync(10, 0).DoWithError(d => actionExectued = true);
+            await AssertionUtilities
+                .DivisionAsync(10, 0)
+                .DoWithError(d => actionExectued = true)
+                .AssertError("Can not divide '10' with '0'.");
 
             Assert.True(actionExectued, "Should not get exectued since there's an error.");
-            Assert.Equal(default, result.Either.Value);
-            Assert.Equal("Can not divide '10' with '0'.", result.Either.Error);
-            Assert.True(result.Either.HasError, "Result should have error.");
-            Assert.False(result.Either.HasValue, "Result should not have value.");
         }
 
         [Fact]
         public async Task
             Result_With_Value__Expects_Action_Not_To_Be_Invoked() {
             var actionExectued = false;
-            var result = await AssertionUtilities.DivisionAsync(10, 2).DoWithError(d => actionExectued = true);
+            await AssertionUtilities
+                .DivisionAsync(10, 2)
+                .DoWithError(d => actionExectued = true)
+                .AssertValue(5);
 
             Assert.False(actionExectued, "Should not get exectued since there's an error.");
-            Assert.Equal(5, result.Either.Value);
-            Assert.Equal(default, result.Either.Error);
-            Assert.False(result.Either.HasError, "Result should not have error.");
-            Assert.True(result.Either.HasValue, "Result should have value.");
         }
     }
 }
