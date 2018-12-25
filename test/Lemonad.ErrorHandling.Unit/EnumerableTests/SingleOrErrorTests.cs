9af48f05973ad5bec660f382e6dcac6150d07843
Robert Andersson <robert.andersson.swe@gmail.com>
2018-12-25 20:33:28 +0100

    Put queryable extensions in seperate namespace

--- test/Lemonad.ErrorHandling.Unit/EnumerableTests/SingleOrErrorTests.cs
+++ test/Lemonad.ErrorHandling.Unit/EnumerableTests/SingleOrErrorTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Assertion;
+using Lemonad.ErrorHandling.Extensions.Result;
 using Lemonad.ErrorHandling.Extensions.Result.Enumerable;
 using Xunit;
 
