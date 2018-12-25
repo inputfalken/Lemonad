e42ed46620e5dbe922f66af3bc8b83a8ed0f265c
Robert Andersson <robert.andersson.swe@gmail.com>
2018-12-25 12:55:15 +0100

    Remove unused extensions

--- src/Lemonad.ErrorHandling/Internal/CompositionExtensions.cs
+++ src/Lemonad.ErrorHandling/Internal/CompositionExtensions.cs
@@ -3,25 +3,19 @@ using System.Threading.Tasks;
 
 namespace Lemonad.ErrorHandling.Internal {
     internal static class CompositionExtensions {
-        public static Func<T2> Compose<T1, T2>(this Func<T1> source, Func<T1, T2> selector) {
-            return () => selector(source());
-        }
+        public static Func<T2> Compose<T1, T2>(
+            this Func<T1> source,
+            Func<T1, T2> selector
+        ) => () => selector(source());
 
-        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> source, Func<T2, T3> selector) {
-            return x => selector(source(x));
-        }
+        public static Func<T1, T3> Compose<T1, T2, T3>(
+            this Func<T1, T2> source,
+            Func<T2, T3> selector
+        ) => x => selector(source(x));
 
-        public static Func<T1, Task<T3>>
-            ComposeAsync<T1, T2, T3>(this Func<T1, Task<T2>> source, Func<T2, T3> selector) {
-            return async x => selector(await source(x).ConfigureAwait(false));
-        }
-    }
-
-    public static class NullableExtensions {
-        public static TResult? Map<T, TResult>(this T? source, Func<T, TResult> selector)
-            where T : struct where TResult : struct =>
-            source.HasValue
-                ? (TResult?) selector(source.Value)
-                : null;
+        public static Func<T1, Task<T3>> ComposeAsync<T1, T2, T3>(
+            this Func<T1, Task<T2>> source,
+            Func<T2, T3> selector
+        ) => async x => selector(await source(x).ConfigureAwait(false));
     }
 }
