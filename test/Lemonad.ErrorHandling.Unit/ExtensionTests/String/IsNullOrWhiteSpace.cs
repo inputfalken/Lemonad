6ba57f3bb2e23d9c2285185dcbf1755c3aa8c81e
Robert Andersson <robert.andersson.swe@gmail.com>
2018-12-31 01:20:16 +0100

    Add tests for string extensions

---
+++ test/Lemonad.ErrorHandling.Unit/ExtensionTests/String/IsNullOrWhiteSpace.cs
@@ -0,0 +1,25 @@
+using Assertion;
+using Lemonad.ErrorHandling.Extensions.String;
+using Xunit;
+
+namespace Lemonad.ErrorHandling.Unit.ExtensionTests.String {
+    public class IsNullOrWhiteSpace {
+        [Fact]
+        public void Empty_String() => string.Empty.IsNullOrWhiteSpace().AssertError(IsNullOrWhiteSpaceCase.Empty);
+
+        [Fact]
+        public void Null_String() => ((string) null).IsNullOrWhiteSpace().AssertError(IsNullOrWhiteSpaceCase.Null);
+
+        [Fact]
+        public void String_With_Word() => "foobar".IsNullOrWhiteSpace().AssertValue("foobar");
+
+        [Fact]
+        public void String_With_Space() => " ".IsNullOrWhiteSpace().AssertError(IsNullOrWhiteSpaceCase.WhiteSpace);
+
+        [Fact]
+        public void String_With_Newline() => "\n".IsNullOrWhiteSpace().AssertError(IsNullOrWhiteSpaceCase.WhiteSpace);
+
+        [Fact]
+        public void String_With_Tab() => "\n".IsNullOrWhiteSpace().AssertError(IsNullOrWhiteSpaceCase.WhiteSpace);
+    }
+}
