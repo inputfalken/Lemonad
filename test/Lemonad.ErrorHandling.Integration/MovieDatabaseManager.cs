4639fc51a50af9ef3cab6d23243ea04fdc1f1264
Robert Andersson <robert.andersson.swe@gmail.com>
2018-12-31 01:20:45 +0100

    Use 4

--- test/Lemonad.ErrorHandling.Integration/MovieDatabaseManager.cs
+++ test/Lemonad.ErrorHandling.Integration/MovieDatabaseManager.cs
@@ -10,7 +10,7 @@ using Sharpy.Core.Linq;
 using Xunit;
 
 // If this is not disabled, all the connection pools will be used up for the database.
-[assembly: CollectionBehavior(MaxParallelThreads = 6)]
+[assembly: CollectionBehavior(MaxParallelThreads = 4)]
 
 namespace Lemonad.ErrorHandling.Integration {
     public interface IDatabaseManager {
