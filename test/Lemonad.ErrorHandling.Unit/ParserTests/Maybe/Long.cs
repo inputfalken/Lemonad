08583b225262ff7d31517057cf78be4bc2d6fa75
Robert Andersson <robert.andersson.swe@gmail.com>
2019-01-01 15:48:44 +0100

    Add Long coverage

--- test/Lemonad.ErrorHandling.Unit/ParserTests/Maybe/Long.cs
+++ test/Lemonad.ErrorHandling.Unit/ParserTests/Maybe/Long.cs
@@ -1,8 +1,27 @@
+using System.Globalization;
 using Assertion;
 using Xunit;
 
 namespace Lemonad.ErrorHandling.Unit.ParserTests.Maybe {
     public class Long {
+        [Fact]
+        public void With_Valid_String_With_NumberStyle()
+            => Parsers.MaybeParsers
+                .Long(long.MaxValue.ToString())
+                .AssertValue(long.MaxValue);
+
+        [Fact]
+        public void With_Invalid_String_With_NumberStyle()
+            => Parsers.MaybeParsers
+                .Long("foobar", NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertNone();
+
+        [Fact]
+        public void With_Null_String_With_NumberStyle()
+            => Parsers.MaybeParsers
+                .Long(null, NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertNone();
+
         [Fact]
         public void With_Valid_String()
             => Parsers.MaybeParsers
--- test/Lemonad.ErrorHandling.Unit/ParserTests/Result/Long.cs
+++ test/Lemonad.ErrorHandling.Unit/ParserTests/Result/Long.cs
@@ -1,12 +1,39 @@
+using System.Globalization;
 using Assertion;
 using Xunit;
 
 namespace Lemonad.ErrorHandling.Unit.ParserTests.Result {
     public class Long {
         [Fact]
-        public void With_Valid_String()
+        public void With_Valid_String_With_NumberStyle()
             => Parsers.ResultParsers
                 .Long(long.MaxValue.ToString())
+                .AssertValue(long.MaxValue);
+
+        [Fact]
+        public void With_Invalid_String_With_NumberStyle()
+            => Parsers.ResultParsers
+                .Long("foobar", NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertError(
+                    AssertionUtilities.FormatStringParserMessage<long>(
+                        "foobar"
+                    )
+                );
+
+        [Fact]
+        public void With_Null_String_With_NumberStyle()
+            => Parsers.ResultParsers
+                .Long(null, NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertError(
+                    AssertionUtilities.FormatStringParserMessage<long>(
+                        null
+                    )
+                );
+
+        [Fact]
+        public void With_Valid_String()
+            => Parsers.ResultParsers
+                .Long(long.MaxValue.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture)
                 .AssertValue(long.MaxValue);
 
         [Fact]
