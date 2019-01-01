25334bc9241ce54ac70bb4ba96f27c4094a385c2
Robert Andersson <robert.andersson.swe@gmail.com>
2019-01-01 15:52:10 +0100

    Add coverage for Int

--- test/Lemonad.ErrorHandling.Unit/ParserTests/Maybe/Int.cs
+++ test/Lemonad.ErrorHandling.Unit/ParserTests/Maybe/Int.cs
@@ -1,8 +1,27 @@
+using System.Globalization;
 using Assertion;
 using Xunit;
 
 namespace Lemonad.ErrorHandling.Unit.ParserTests.Maybe {
     public class Int {
+        [Fact]
+        public void With_Valid_String_With_NumberStyle()
+            => Parsers.MaybeParsers
+                .Int("20", NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertValue(20);
+
+        [Fact]
+        public void With_Invalid_String_With_NumberStyle()
+            => Parsers.MaybeParsers
+                .Int("foobar", NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertNone();
+
+        [Fact]
+        public void With_Null_String_With_NumberStyle()
+            => Parsers.MaybeParsers
+                .Int(null, NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertNone();
+
         [Fact]
         public void With_Valid_String()
             => Parsers.MaybeParsers
--- test/Lemonad.ErrorHandling.Unit/ParserTests/Result/Int.cs
+++ test/Lemonad.ErrorHandling.Unit/ParserTests/Result/Int.cs
@@ -1,8 +1,35 @@
+using System.Globalization;
 using Assertion;
 using Xunit;
 
 namespace Lemonad.ErrorHandling.Unit.ParserTests.Result {
     public class Int {
+        [Fact]
+        public void With_Valid_String_With_NumberStyle()
+            => Parsers.ResultParsers
+                .Int("20", NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertValue(20);
+
+        [Fact]
+        public void With_Invalid_String_With_NumberStyle()
+            => Parsers.ResultParsers
+                .Int("foobar", NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertError(
+                    AssertionUtilities.FormatStringParserMessage<int>(
+                        "foobar"
+                    )
+                );
+
+        [Fact]
+        public void With_Null_String_With_NumberStyle()
+            => Parsers.ResultParsers
+                .Int(null, NumberStyles.Any, CultureInfo.InvariantCulture)
+                .AssertError(
+                    AssertionUtilities.FormatStringParserMessage<int>(
+                        null
+                    )
+                );
+
         [Fact]
         public void With_Valid_String()
             => Parsers.ResultParsers
