b90f5ca1b7ed60bd8637779b624f054526c4c88d
Robert Andersson <robert.andersson.swe@gmail.com>
2019-01-01 15:38:31 +0100

    Add coverage for DateTime

--- test/Lemonad.ErrorHandling.Unit/ParserTests/Result/DateTime.cs
+++ test/Lemonad.ErrorHandling.Unit/ParserTests/Result/DateTime.cs
@@ -29,5 +29,32 @@ namespace Lemonad.ErrorHandling.Unit.ParserTests.Result {
                         null
                     )
                 );
+
+        [Fact]
+        public void With_Valid_String_With_DateTimeStyle()
+            => Parsers.ResultParsers
+                .DateTime(System.DateTime.Today.ToString(CultureInfo.InvariantCulture), DateTimeStyles.None,
+                    CultureInfo.InvariantCulture)
+                .AssertValue(System.DateTime.Today);
+
+        [Fact]
+        public void With_Invalid_String_With_DateTimeStyle()
+            => Parsers.ResultParsers
+                .DateTime("foobar", DateTimeStyles.None, CultureInfo.InvariantCulture)
+                .AssertError(
+                    AssertionUtilities.FormatStringParserMessage<System.DateTime>(
+                        "foobar"
+                    )
+                );
+
+        [Fact]
+        public void With_Null_String_With_DateTimeStyle()
+            => Parsers.ResultParsers
+                .DateTime(null, DateTimeStyles.None, CultureInfo.InvariantCulture)
+                .AssertError(
+                    AssertionUtilities.FormatStringParserMessage<System.DateTime>(
+                        null
+                    )
+                );
     }
 }
