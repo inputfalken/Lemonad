3142272144a6b18ca075a01fa73970f6b718f8c1
Robert Andersson <robert.andersson.swe@gmail.com>
2019-01-04 16:52:32 +0100

    Add test coverage for fullmap & fullmapasync

--- test/Assertion/AssertionUtilities.cs
+++ test/Assertion/AssertionUtilities.cs
@@ -21,6 +21,11 @@ namespace Assertion {
         public static IMaybe<int> DivisionMaybe(int left, int right) =>
             right != 0 ? Maybe.Value(left / right) : Maybe.None<int>();
 
+        public static async Task<string> GetTask(string message) {
+            await Delay;
+            return message;
+        }
+
         public static IResult<double, string> Division(double left, double right) => (left, right).ToResult(
                 x => right != 0,
                 x => $"Can not divide '{x.left}' with '{x.right}'."
--- test/Lemonad.ErrorHandling.Unit/AsyncResult.Tests/FullMapTests.cs
+++ test/Lemonad.ErrorHandling.Unit/AsyncResult.Tests/FullMapTests.cs
@@ -1,10 +1,25 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Assertion;
 using Lemonad.ErrorHandling.Extensions.AsyncResult;
 using Xunit;
 
 namespace Lemonad.ErrorHandling.Unit.AsyncResult.Tests {
     public class FullMapTests {
+        [Fact]
+        public void Passing_Null_Selector_Throws()
+            => Assert.Throws<ArgumentNullException>(
+                AssertionUtilities.SelectorName,
+                () => AssertionUtilities.Division(10, 2).FullMap<string, string>(null, s => s)
+            );
+
+        [Fact]
+        public void Passing_Null_ErrorSelector_Throws()
+            => Assert.Throws<ArgumentNullException>(
+                AssertionUtilities.ErrorSelectorName,
+                () => AssertionUtilities.Division(10, 2).FullMap<string, string>(_ => string.Empty, null)
+            );
+
         [Fact]
         public async Task Result_With_Error_Expects__Selector_Never__To_Be_Executed_And_ErrorSelector_To_Be_Invoked() {
             var selectorExectued = false;
@@ -17,50 +32,7 @@ namespace Lemonad.ErrorHandling.Unit.AsyncResult.Tests {
                 }, s => {
                     errorSelectorExectued = true;
                     return s.ToUpper();
-                })
-                .AssertError("CAN NOT DIVIDE '10' WITH '0'.");
-
-            Assert.False(selectorExectued, "Should not get exectued since there's an error from the result.");
-            Assert.True(errorSelectorExectued, "Should get exectued since there's an error from the result.");
-        }
-
-        [Fact]
-        public async Task
-            Result_With_Error_Expects__Selector_Never__To_Be_Executed_And_ErrorSelector_To_Be_Invoked_With_async_Error() {
-            var selectorExectued = false;
-            var errorSelectorExectued = false;
-            await AssertionUtilities
-                .DivisionAsync(10, 0)
-                .FullMapAsync(d => {
-                    selectorExectued = true;
-                    return d * 2;
-                }, async s => {
-                    await Task.Delay(50);
-                    errorSelectorExectued = true;
-                    return s.ToUpper();
-                })
-                .AssertError("CAN NOT DIVIDE '10' WITH '0'.");
-
-            Assert.False(selectorExectued, "Should not get exectued since there's an error from the result.");
-            Assert.True(errorSelectorExectued, "Should get exectued since there's an error from the result.");
-        }
-
-        [Fact]
-        public async Task
-            Result_With_Error_Expects__Selector_Never__To_Be_Executed_And_ErrorSelector_To_Be_Invoked_With_async_Value() {
-            var selectorExectued = false;
-            var errorSelectorExectued = false;
-            await AssertionUtilities
-                .DivisionAsync(10, 0)
-                .FullMapAsync(async d => {
-                    await Task.Delay(50);
-                    selectorExectued = true;
-                    return d * 2;
-                }, s => {
-                    errorSelectorExectued = true;
-                    return s.ToUpper();
-                })
-                .AssertError("CAN NOT DIVIDE '10' WITH '0'.");
+                }).AssertError("CAN NOT DIVIDE '10' WITH '0'.");
 
             Assert.False(selectorExectued, "Should not get exectued since there's an error from the result.");
             Assert.True(errorSelectorExectued, "Should get exectued since there's an error from the result.");
@@ -79,48 +51,6 @@ namespace Lemonad.ErrorHandling.Unit.AsyncResult.Tests {
                     errorSelectorExectued = true;
                     return s.ToUpper();
                 }).AssertValue(50);
-
-            Assert.True(selectorExectued, "Should get exectued since there's an value from the result.");
-            Assert.False(errorSelectorExectued, "Should not get exectued since there's an value from the result.");
-        }
-
-        [Fact]
-        public async Task
-            Result_With_Value_Expects__Selector_To_Be_Executed_And_ErrorSelector_To_Never_Be_Invoked_With_async_Error() {
-            var selectorExectued = false;
-            var errorSelectorExectued = false;
-            await AssertionUtilities
-                .DivisionAsync(10, 2)
-                .FullMapAsync(d => {
-                    selectorExectued = true;
-                    return d * 10;
-                }, async s => {
-                    await Task.Delay(50);
-                    errorSelectorExectued = true;
-                    return s.ToUpper();
-                })
-                .AssertValue(50);
-
-            Assert.True(selectorExectued, "Should get exectued since there's an value from the result.");
-            Assert.False(errorSelectorExectued, "Should not get exectued since there's an value from the result.");
-        }
-
-        [Fact]
-        public async Task
-            Result_With_Value_Expects__Selector_To_Be_Executed_And_ErrorSelector_To_Never_Be_Invoked_With_async_Value() {
-            var selectorExectued = false;
-            var errorSelectorExectued = false;
-            await AssertionUtilities
-                .DivisionAsync(10, 2)
-                .FullMapAsync(async d => {
-                    await Task.Delay(50);
-                    selectorExectued = true;
-                    return d * 10;
-                }, s => {
-                    errorSelectorExectued = true;
-                    return s.ToUpper();
-                })
-                .AssertValue(50);
 
             Assert.True(selectorExectued, "Should get exectued since there's an value from the result.");
             Assert.False(errorSelectorExectued, "Should not get exectued since there's an value from the result.");
