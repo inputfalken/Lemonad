eaa8ad81e25c7a286d8cb7339d5dabdb9e7e9ed6
Robert Andersson <robert.andersson.swe@gmail.com>
2019-01-06 01:43:29 +0100

    Add missing branch test for safecast

--- test/Lemonad.ErrorHandling.Unit/AsyncResult.Tests/SafeCastTests.cs
+++ test/Lemonad.ErrorHandling.Unit/AsyncResult.Tests/SafeCastTests.cs
@@ -1,8 +1,17 @@
+using System;
 using Assertion;
 using Xunit;
 
 namespace Lemonad.ErrorHandling.Unit.AsyncResult.Tests {
     public class SafeCastTests {
+        [Fact]
+        public void Passing_Null_ErrorSelector_Throws() {
+            Assert.Throws<ArgumentNullException>(
+                AssertionUtilities.ErrorSelectorName,
+                () => AssertionUtilities.GetGenderAsync(2).SafeCast<int>(null)
+            );
+        }
+
         [Fact]
         public void Result_With_Error_Using_Invalid_Cast_Expects_No_Exception() {
             const int identity = 3;
