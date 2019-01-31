186fcd8e059715ef93c79635127ae347b4ea3b55
Robert Andersson <robert.andersson.swe@gmail.com>
2019-01-31 22:00:27 +0100

    Add example docs for GuidFormat enum

--- src/Lemonad.ErrorHandling/Parsers/GuidFormat.cs
+++ src/Lemonad.ErrorHandling/Parsers/GuidFormat.cs
@@ -6,27 +6,42 @@ namespace Lemonad.ErrorHandling.Parsers {
         /// <summary>
         ///     32 digits:
         /// </summary>
+        /// <para>
+        /// Example: 00000000000000000000000000000000
+        /// </para>
         DigitsOnly = 'N',
 
         /// <summary>
         ///     32 digits separated by hyphens:
         /// </summary>
+        /// <para>
+        /// Example: 00000000-0000-0000-0000-000000000000
+        /// </para>
         DigitsWithHyphens = 'D',
 
         /// <summary>
         ///     32 digits separated by hyphens, enclosed in braces:
         /// </summary>
+        /// <para>
+        /// Example: {00000000-0000-0000-0000-000000000000}
+        /// </para>
         DigitsWithHyphensWrappedInBrackets = 'B',
 
         /// <summary>
         ///     32 digits separated by hyphens, enclosed in parentheses:
         /// </summary>
+        /// <para>
+        /// Example: (00000000-0000-0000-0000-000000000000)
+        /// </para>
         DigitsWithHyphensWrappedInParentheses = 'P',
 
         /// <summary>
         ///     Four hexadecimal values enclosed in braces, where the fourth value is a subset of eight hexadecimal values that is
         ///     also enclosed in braces:
         /// </summary>
+        /// <para>
+        /// Example: {0x00000000,0x0000,0x0000,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}}
+        /// </para>
         FourHexadecimalWrappedInBrackets = 'X'
     }
 }
