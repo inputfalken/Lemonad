bcefa60ccacb5b76b9405932e76d352387fa29ad
Robert Andersson <robert.andersson.swe@gmail.com>
2019-02-02 12:56:34 +0100

    Reference IMaybe in comments

--- src/Lemonad.ErrorHandling/Maybe.cs
+++ src/Lemonad.ErrorHandling/Maybe.cs
@@ -1,4 +1,5 @@
-﻿using Lemonad.ErrorHandling.Internal;
+﻿
+using Lemonad.ErrorHandling.Internal;
 
 namespace Lemonad.ErrorHandling {
     /// <summary>
@@ -6,27 +7,27 @@ namespace Lemonad.ErrorHandling {
     /// </summary>
     public static class Maybe {
         /// <summary>
-        ///     Creates a <see cref="Maybe{T}" /> who will have no value.
+        ///     Creates a <see cref="IMaybe{T}" /> who will have no value.
         /// </summary>
         /// <typeparam name="TSource">
-        ///     The type of the <see cref="Maybe{T}" />.
+        ///     The type of the <see cref="IMaybe{T}" />.
         /// </typeparam>
         /// <returns>
-        ///     A <see cref="Maybe{T}" /> who will have no value.
+        ///     A <see cref="IMaybe{T}" /> who will have no value.
         /// </returns>
         public static IMaybe<TSource> None<TSource>() => Maybe<TSource>.None;
 
         /// <summary>
-        ///     Creates a <see cref="Maybe{T}" /> who will have the value <paramref name="item" />.
+        ///     Creates a <see cref="IMaybe{T}" /> who will have the value <paramref name="item" />.
         /// </summary>
         /// <param name="item">
-        ///     The value of <see cref="Maybe{T}" />.
+        ///     The value of <see cref="IMaybe{T}" />.
         /// </param>
         /// <typeparam name="TSource">
-        ///     The type of the <see cref="Maybe{T}" />.
+        ///     The type of the <see cref="IMaybe{T}" />.
         /// </typeparam>
         /// <returns>
-        ///     A <see cref="Maybe{T}" /> whose value will be <paramref name="item" />.
+        ///     A <see cref="IMaybe{T}" /> whose value will be <paramref name="item" />.
         /// </returns>
         public static IMaybe<TSource> Value<TSource>(TSource item) => Maybe<TSource>.Create(item);
     }
