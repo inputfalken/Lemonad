2ef3c471a124668b0ebed8f49fd1a5e3a4b3491a
Robert Andersson <robert.andersson.swe@gmail.com>
2019-02-10 23:34:53 +0100

    ADd flatten coverage

--- test/Lemonad.ErrorHandling.Unit/AsyncMaybe.Tests/FlattenAsyncTests.cs
+++ test/Lemonad.ErrorHandling.Unit/AsyncMaybe.Tests/FlattenAsyncTests.cs
@@ -4,26 +4,26 @@ using Assertion;
 using Lemonad.ErrorHandling.Extensions.AsyncMaybe;
 using Xunit;
 
-namespace Lemonad.ErrorHandling.Unit.Maybe.Tests {
+namespace Lemonad.ErrorHandling.Unit.AsyncMaybe.Tests {
     public class FlattenAsyncTests {
         [Fact]
         public async Task
             Flattening_From_String_Maybe_With_No_Value_To_Maybe_Int_With_No_Value__Expects_String_Maybe_With_Value() {
-            await ErrorHandling.Maybe.None<string>()
+            await ErrorHandling.AsyncMaybe.None<string>()
                 .FlattenAsync(x => Lemonad.ErrorHandling.AsyncMaybe.None<int>()).AssertNone();
         }
 
         [Fact]
         public async Task
             Flattening_From_String_Maybe_With_No_Value_To_Maybe_Int_With_Value__Expects_String_Maybe_With_Value() {
-            await ErrorHandling.Maybe.None<string>()
+            await ErrorHandling.AsyncMaybe.None<string>()
                 .FlattenAsync(x => Lemonad.ErrorHandling.AsyncMaybe.Value(2)).AssertNone();
         }
 
         [Fact]
         public async Task
             Flattening_From_String_Maybe_With_Value_To_Maybe_Int_With_No_Value__Expects_String_Maybe_With_Value() {
-            await ErrorHandling.Maybe.Value("hello")
+            await ErrorHandling.AsyncMaybe.Value("hello")
                 .FlattenAsync(x => Lemonad.ErrorHandling.AsyncMaybe.None<int>())
                 .AssertNone();
         }
@@ -32,7 +32,7 @@ namespace Lemonad.ErrorHandling.Unit.Maybe.Tests {
         public async Task
             Flattening_From_String_Maybe_With_Value_To_Maybe_Int_With_Value__Expects_String_Maybe_With_Value() {
             const string input = "hello";
-            await ErrorHandling.Maybe.Value(input)
+            await ErrorHandling.AsyncMaybe.Value(input)
                 .FlattenAsync(x => Lemonad.ErrorHandling.AsyncMaybe.Value(2))
                 .AssertValue(input);
         }
@@ -40,6 +40,6 @@ namespace Lemonad.ErrorHandling.Unit.Maybe.Tests {
         [Fact]
         public void Passing_Null_Selector_Throws() =>
             Assert.Throws<ArgumentNullException>(AssertionUtilities.SelectorName,
-                () => ErrorHandling.Maybe.Value(2).FlattenAsync((Func<int, IAsyncMaybe<int>>) null));
+                () => ErrorHandling.AsyncMaybe.Value(2).FlattenAsync((Func<int, IAsyncMaybe<int>>) null));
     }
 }
--- test/Lemonad.ErrorHandling.Unit/AsyncMaybe.Tests/FlattenTests.cs
+++ test/Lemonad.ErrorHandling.Unit/AsyncMaybe.Tests/FlattenTests.cs
@@ -1,42 +1,46 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Assertion;
 using Lemonad.ErrorHandling.Extensions;
+using Lemonad.ErrorHandling.Extensions.AsyncMaybe;
 using Xunit;
 
-namespace Lemonad.ErrorHandling.Unit.Maybe.Tests {
+namespace Lemonad.ErrorHandling.Unit.AsyncMaybe.Tests {
     public class FlattenTests {
         [Fact]
-        public void
+        public async Task
             Flattening_From_String_Maybe_With_No_Value_To_Maybe_Int_With_No_Value__Expects_String_Maybe_With_Value() {
-            ErrorHandling.Maybe.None<string>().AssertNone().Flatten(x => 2.ToMaybeNone().AssertNone()).AssertNone();
-        }
-
-        [Fact]
-        public void
-            Flattening_From_String_Maybe_With_No_Value_To_Maybe_Int_With_Value__Expects_String_Maybe_With_Value() {
-            ErrorHandling.Maybe.None<string>().Flatten(x => ErrorHandling.Maybe.Value(2).AssertValue(2)).AssertNone();
-        }
-
-        [Fact]
-        public void
-            Flattening_From_String_Maybe_With_Value_To_Maybe_Int_With_No_Value__Expects_String_Maybe_With_Value() {
-            ErrorHandling.Maybe.Value("hello")
-                .Flatten(x => 2.ToMaybeNone().AssertNone())
+            await ErrorHandling.AsyncMaybe.None<string>().AssertNone().Flatten(x => ErrorHandling.Maybe.None<int>())
                 .AssertNone();
         }
 
         [Fact]
-        public void
+        public async Task
+            Flattening_From_String_Maybe_With_No_Value_To_Maybe_Int_With_Value__Expects_String_Maybe_With_Value() {
+            await ErrorHandling.AsyncMaybe.None<string>().Flatten(x => ErrorHandling.Maybe.Value(2))
+                .AssertNone();
+        }
+
+        [Fact]
+        public async Task
+            Flattening_From_String_Maybe_With_Value_To_Maybe_Int_With_No_Value__Expects_String_Maybe_With_Value() {
+            await ErrorHandling.AsyncMaybe.Value("hello")
+                .Flatten(x => ErrorHandling.Maybe.None<int>())
+                .AssertNone();
+        }
+
+        [Fact]
+        public async Task
             Flattening_From_String_Maybe_With_Value_To_Maybe_Int_With_Value__Expects_String_Maybe_With_Value() {
             const string input = "hello";
-            ErrorHandling.Maybe.Value(input)
-                .Flatten(x => ErrorHandling.Maybe.Value(2).AssertValue(2))
+            await ErrorHandling.AsyncMaybe.Value(input)
+                .Flatten(x => ErrorHandling.Maybe.Value(2))
                 .AssertValue(input);
         }
 
         [Fact]
         public void Passing_Null_Selector_Throws() =>
             Assert.Throws<ArgumentNullException>(AssertionUtilities.SelectorName,
-                () => ErrorHandling.Maybe.Value(2).Flatten((Func<int, IMaybe<int>>) null));
+                () => ErrorHandling.AsyncMaybe.Value(2).Flatten((Func<int, IMaybe<int>>) null));
     }
 }
