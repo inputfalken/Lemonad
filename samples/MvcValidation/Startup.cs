86232980ff170f4ddbe6c5b399ffdcdbd2b67c9e
Robert Andersson <robert.andersson.swe@gmail.com>
2019-02-16 13:56:07 +0100

    Update dependencies

--- samples/MvcValidation/Startup.cs
+++ samples/MvcValidation/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Lemonad.ErrorHandling;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
